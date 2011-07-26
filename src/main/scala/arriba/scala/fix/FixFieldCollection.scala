e99cb83d9f4d168281b9754d890432f12f66a3b7
Noah Cornwell <noah0078@gmail.com>
2011-07-25 21:58:26 -0400

    Adding some fix files

---
+++ src/main/scala/arriba/scala/fix/FixFieldCollection.scala
@@ -0,0 +1,13 @@
+package arriba.scala.fix
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ncornwell
+ * Date: 7/25/11
+ * Time: 9:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class FixFieldCollection {
+
+}
