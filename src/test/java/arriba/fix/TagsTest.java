4556a4a3d289a23769c4b72c8c4cdf0dc3765f32
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-07-26 22:10:35 -0400

    * Expose new Tags convenience method toDelimitedByteArray to produce byte array that prepends the FIX field delimiter. * Refactor FixMessageFrameDecoder.decode() implementation to return * unmodified byte array representing entire FIX message.  This approach minimizes object creation even further than the previous approach.

--- src/test/java/arriba/fix/TagsTest.java
+++ src/test/java/arriba/fix/TagsTest.java
@@ -7,16 +7,24 @@ import org.junit.Test;
 
 public class TagsTest {
 
+    private final int tag = 100;
+
     @Test
     public void testToByteArray() {
-        final int tag = 100;
+        final byte[] expectedTagBytes = Integer.toString(this.tag).getBytes();
+        final byte[] actualTagBytes = Tags.toByteArray(this.tag);
 
-        final byte[] undelimitedTagBytes = Integer.toString(tag).getBytes();
+        assertThat(actualTagBytes, is(expectedTagBytes));
+    }
+
+    @Test
+    public void testToDelimitedByteArray() {
+        final byte[] undelimitedTagBytes = Integer.toString(this.tag).getBytes();
         final byte[] expectedTagBytes = new byte[1 + undelimitedTagBytes.length];
         expectedTagBytes[0] = Fields.DELIMITER;
         System.arraycopy(undelimitedTagBytes, 0, expectedTagBytes, 1, undelimitedTagBytes.length);
 
-        final byte[] actualTagBytes = Tags.toByteArray(tag);
+        final byte[] actualTagBytes = Tags.toDelimitedByteArray(this.tag);
 
         assertThat(actualTagBytes, is(expectedTagBytes));
     }
