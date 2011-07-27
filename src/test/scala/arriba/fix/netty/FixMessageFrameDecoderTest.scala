a8fa796696fa8491004cf4e0d13e3364c43feb23
dan <dan@control.(none)>
2011-07-27 19:29:27 -0400

    adding sbt

---
+++ src/test/scala/arriba/fix/netty/FixMessageFrameDecoderTest.scala
@@ -0,0 +1,14 @@
+package arriba.fix.netty
+
+import org.specs2.mutable._
+// Copyright 2011 Billco Technology.  All rights reserved.
+
+class FixMessageFrameDecoderTest extends Specification {
+
+  "Fix Message Frame Decoder" should {
+    "take 1.5 messages" in {
+      success
+    }
+  }
+
+}
