d5a24c1f83860754b2eb7d79e32e74fa3b8ad186
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-07-27 22:38:17 -0400

    Introduce FixServerBootstrap and FixClientBootstrap to facilitate creation of Netty FIX client and servers for production and testing use cases.

---
+++ src/main/java/arriba/server/FixClientBootstrap.java
@@ -0,0 +1,31 @@
+package arriba.server;
+
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelHandler;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+public final class FixClientBootstrap {
+
+    public static ClientBootstrap create(final ChannelHandler... channelHandlers) {
+        final ChannelFactory factory = new NioClientSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+
+        final ClientBootstrap bootstrap = new ClientBootstrap(factory);
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() {
+                return Channels.pipeline(channelHandlers);
+            }
+        });
+
+        bootstrap.setOption("tcpNoDelay", true);
+        bootstrap.setOption("keepAlive", true);
+
+        return bootstrap;
+    }
+}
---
+++ src/main/java/arriba/server/FixServerBootstrap.java
@@ -0,0 +1,32 @@
+package arriba.server;
+
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelHandler;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public final class FixServerBootstrap {
+
+    public static ServerBootstrap create(final ChannelHandler... channelHandlers) {
+        final ChannelFactory factory =
+            new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+
+        final ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() {
+                return Channels.pipeline(channelHandlers);
+            }
+        });
+
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+
+        return bootstrap;
+    }
+}
