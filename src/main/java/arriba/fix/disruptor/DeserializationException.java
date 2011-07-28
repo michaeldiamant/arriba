73f55d87d88cd8073dda66e1b61d9cc0eae741bd
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-07-27 22:37:53 -0400

    Introduce DeserializationException to handle cases where deserialization is not possible.

---
+++ src/main/java/arriba/fix/disruptor/DeserializationException.java
@@ -0,0 +1,10 @@
+package arriba.fix.disruptor;
+
+public final class DeserializationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public DeserializationException(final String message) {
+        super(message);
+    }
+}
