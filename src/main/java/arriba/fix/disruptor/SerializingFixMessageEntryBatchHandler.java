572caa23fd43b03c5c96fb9e85ca2eb83a9262fd
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-05 10:54:22 -0400

    Remove @Override annotation.

--- src/main/java/arriba/fix/disruptor/SerializingFixMessageEntryBatchHandler.java
+++ src/main/java/arriba/fix/disruptor/SerializingFixMessageEntryBatchHandler.java
@@ -7,16 +7,14 @@ import arriba.fix.messages.FixMessage;
 import com.lmax.disruptor.BatchHandler;
 
 public final class SerializingFixMessageEntryBatchHandler implements BatchHandler<FixMessageEntry> {
-	
-	public SerializingFixMessageEntryBatchHandler() {}
-	
-	@Override
-	public void onAvailable(final FixMessageEntry entry) throws Exception {
-		final FixMessage fixMessage = entry.getFixMessage();
-		
-		entry.setSerializedFixMessage(ChannelBuffers.copiedBuffer(fixMessage.toByteArray()));
-	}
-	
-	@Override
-	public void onEndOfBatch() throws Exception {}
+
+    public SerializingFixMessageEntryBatchHandler() {}
+
+    public void onAvailable(final FixMessageEntry entry) throws Exception {
+        final FixMessage fixMessage = entry.getFixMessage();
+
+        entry.setSerializedFixMessage(ChannelBuffers.copiedBuffer(fixMessage.toByteArray()));
+    }
+
+    public void onEndOfBatch() throws Exception {}
 }
