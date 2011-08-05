f530cbfc02d2979bcdc3966ac3ccf458886f9653
mdiamant <diamant.michael@gmail.com>
2011-08-05 10:48:13 -0400

    Modify FixFieldCollection to support header, body, and trailer fields.

--- src/main/java/arriba/fix/FixFieldCollection.java
+++ src/main/java/arriba/fix/FixFieldCollection.java
@@ -2,27 +2,41 @@ package arriba.fix;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 // TODO Implement Map<K, V>.
 public class FixFieldCollection {
 
-    private final int[] tagArray;
-    private final String[] valueArray;
+    private final int[] headerTagArray;
+    private final String[] headerValueArray;
+    private final int[] bodyTagArray;
+    private final String[] bodyValueArray;
+    private final int[] trailerTagArray;
+    private final String[] trailerValueArray;
 
-    private FixFieldCollection(final List<Field<String>> fields) {
-        Collections.sort(fields);
+    private FixFieldCollection(final List<Field<String>> headerFields, final List<Field<String>> bodyFields,
+            final List<Field<String>> trailerFields) {
+        this.headerTagArray = new int[headerFields.size()];
+        this.headerValueArray = new String[headerFields.size()];
+        populate(this.headerTagArray, this.headerValueArray, headerFields);
 
-        this.tagArray = new int[fields.size()];
-        this.valueArray = new String[fields.size()];
+        this.bodyTagArray = new int[bodyFields.size()];
+        this.bodyValueArray = new String[bodyFields.size()];
+        populate(this.bodyTagArray, this.bodyValueArray, bodyFields);
 
+        this.trailerTagArray = new int[trailerFields.size()];
+        this.trailerValueArray = new String[trailerFields.size()];
+        populate(this.trailerTagArray, this.trailerValueArray, trailerFields);
+    }
+
+    private static void populate(final int[] tagArray, final String[] valueArray, final List<Field<String>> fields) {
         int arrayIndex = 0;
         for (final Field<String> field : fields) {
-            this.tagArray[arrayIndex] = field.getTag();
-            this.valueArray[arrayIndex] = field.getValue();
+            tagArray[arrayIndex] = field.getTag();
+            valueArray[arrayIndex] = field.getValue();
             ++arrayIndex;
         }
     }
@@ -31,22 +45,10 @@ public class FixFieldCollection {
         // TODO Performance test ByteArrayOutputStream vs building LinkedList of all bytes to be written
         // and performing one allocation / write.
         try {
-            int tag;
-            String value;
             final ByteArrayOutputStream messageBytes = new ByteArrayOutputStream();
-            for (int tagIndex = 0; tagIndex < this.tagArray.length; tagIndex++) {
-                tag = this.tagArray[tagIndex];
-                value = this.valueArray[tagIndex];
-
-                final byte[] tagBytes = Tags.toDelimitedByteArray(tag);
-                final byte[] valueBytes = value.getBytes();
-
-                messageBytes.write(tagBytes);
-                messageBytes.write(Fields.EQUAL_SIGN);
-                messageBytes.write(valueBytes);
-
-            }
-            messageBytes.write(Fields.DELIMITER);
+            write(this.headerTagArray, this.headerValueArray, messageBytes);
+            write(this.bodyTagArray, this.bodyValueArray, messageBytes);
+            write(this.trailerTagArray, this.trailerValueArray, messageBytes);
 
             return messageBytes.toByteArray();
         } catch (final IOException e) {
@@ -55,27 +57,81 @@ public class FixFieldCollection {
         }
     }
 
-    public String getValue(final int tag) {
-        final int valueIndex = Arrays.binarySearch(this.tagArray, tag);
+    private static void write(final int[] tagArray, final String[] valueArray, final OutputStream outputStream) throws IOException {
+        for (int tagIndex = 0; tagIndex < tagArray.length; tagIndex++) {
+            final int tag = tagArray[tagIndex];
+            final String value = valueArray[tagIndex];
+
+            final byte[] tagBytes = Tags.toByteArray(tag);
+            final byte[] valueBytes = value.getBytes();
+
+            outputStream.write(tagBytes);
+            outputStream.write(Fields.EQUAL_SIGN);
+            outputStream.write(valueBytes);
+            outputStream.write(Fields.DELIMITER);
+        }
+    }
+
+    private static String search(final int[] tagArray, final String[] valueArray, final int tag) {
+        final int valueIndex = Arrays.binarySearch(tagArray, tag);
 
         // TODO Handle not finding a tag.
-        return valueIndex < 0 ? "" : this.valueArray[valueIndex];
+        return valueIndex < 0 ? "" : valueArray[valueIndex];
+    }
+
+    public String getTrailerValue(final int tag) {
+        return search(this.trailerTagArray, this.trailerValueArray, tag);
+    }
+
+    public String getBodyValue(final int tag) {
+        return search(this.bodyTagArray, this.bodyValueArray, tag);
+    }
+
+    public String getHeaderValue(final int tag) {
+        return search(this.headerTagArray, this.headerValueArray, tag);
+    }
+
+    public String getValue(final int tag) {
+        final String headerValue = this.getHeaderValue(tag);
+        if (!headerValue.isEmpty()) {
+            return headerValue;
+        }
+
+        final String bodyValue = this.getBodyValue(tag);
+        if (!bodyValue.isEmpty()) {
+            return bodyValue;
+        }
+
+        return this.getTrailerValue(tag);
     }
 
     public static class Builder {
 
-        private final List<Field<String>> fields = new LinkedList<Field<String>>();
+        private static final int[] HEADER_TAGS = Tags.getHeaders();
+        private static final int[] TRAILER_TAGS = Tags.getTrailers();
+
+        private final List<Field<String>> headerFields = new LinkedList<Field<String>>();
+        private final List<Field<String>> bodyFields = new LinkedList<Field<String>>();
+        private final List<Field<String>> trailerFields = new LinkedList<Field<String>>();
 
         public Builder() {}
 
         public Builder addField(final int tag, final String value) {
-            this.fields.add(new Field<String>(tag, value));
+            final Field<String> field = new Field<String>(tag, value);
+
+            if (Arrays.binarySearch(HEADER_TAGS, tag) >= 0) {
+                this.headerFields.add(field);
+            } else if (Arrays.binarySearch(TRAILER_TAGS, tag) >= 0) {
+                this.trailerFields.add(field);
+            } else {
+                this.bodyFields.add(field);
+            }
 
             return this;
         }
 
         public FixFieldCollection build() {
-            return new FixFieldCollection(this.fields);
+            return new FixFieldCollection(this.headerFields, this.bodyFields, this.trailerFields);
         }
     }
 }
