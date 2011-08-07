035cc2893ee28fd959baea776e1bfb409cc37074
Dan Billings <danbills@live.com>
2011-08-07 16:19:38 -0400

    parsing for data fields

---
+++ src/main/scala/arriba/fix/parsers/FixParserNoData.scala
@@ -0,0 +1,60 @@
+package arriba.fix.parsers
+
+import util.parsing.combinator.RegexParsers
+import javax.swing.text.html.parser.Parser
+
+/**
+ * This class is suitable for fix messages without data fields
+ */
+class FixParserNoData extends RegexParsers {
+
+  def fixMessage = rep(field)
+
+
+  def tag:Parser[Int] ="""[1-9][\d]*""".r ^^ (_.toInt)
+
+  def field: Parser[(Int, String)] = tag ~ "=" ~ """[^\001]*""".r ~ "\001" ^^ { case tagResult ~ eq ~ valueResult ~ delimiter => (tagResult,valueResult) }
+
+  def value:Parser[String] = """[^\001]*""".r
+}
+
+/**
+ * <SOH> is no longer the universal delimiter when dealing with data fields.
+ */
+class FixParserData extends RegexParsers {
+  def dataField() = dataLengthEntirety >> { entireDataField => repN(1, """.""".r) }
+  def dataLengthEntirety = rep("""[^\001]""".r)
+  def shards = rep("""[^\001]*\001""".r)
+}
+
+class RefineShardsIntoOneMessage extends FixParserNoData {
+  this:FixParserNoData =>
+  def isCheckSumField(shard:String) : Boolean = {
+    parseAll(field, shard ) match {
+      case this.Success(result, next) => true
+      case _ => false
+    }
+
+  }
+  def apply(shards: List[String]) : Option[(List[String], List[String])] = shards.find(isCheckSumField(_)) match {
+    case Some(checkSumField) => {
+      Some(("" :: Nil, "" :: Nil))
+    }
+
+  }
+}
+
+class ArrangeShardsIntoFields extends FixParserNoData {
+  def subshardsWithLeadingDataLengthField:Parser[(Int, Int, String)] = tag ~ "=" ~ """"\d*\001""".r ~ """.""".r ^^ {
+    case tag ~ equals ~ value ~ rest => (tag, value.toInt, rest)
+  }
+
+  def apply(shards: List[String]): List[(Int, String)] = {
+    val span = shards.span(shard => parseAll(subshardsWithLeadingDataLengthField, shard) match {
+      case this.Success(result, next) => true
+      case _ => false
+    })
+
+    (4, "") :: Nil
+  }
+}
