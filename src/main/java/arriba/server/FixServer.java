32d45d610f219345bb3eb8576cb553fe324cc807
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-14 19:06:56 -0400

    * Provide ChannelWritingFixMessageEntryBatchHandler to serialize a FIX message and write it to the wire via Netty Channel. * Wire together FixServer to support FIX message deserialization (incoming) and FIX message serialization/transmission (outgoing). * Remove unnecessary handler SerializingFixMessageEntryBatchHandler. * Remove out of date test SerializingFixMessageEntryBatchHandlerTest.  It will be replaced in a subsequent commit.

--- src/main/java/arriba/server/FixServer.java
+++ src/main/java/arriba/server/FixServer.java
@@ -9,11 +9,20 @@ import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.ChannelHandler;
 
 import arriba.common.Sender;
+import arriba.fix.FixMessageBuilder;
+import arriba.fix.chunk.arrays.ArrayFixChunk;
+import arriba.fix.chunk.arrays.ArrayFixChunkBuilder;
+import arriba.fix.disruptor.ChannelWritingFixMessageEntryBatchHandler;
+import arriba.fix.disruptor.DeserializingFixMessageEntryBatchHandler;
 import arriba.fix.disruptor.FixMessageEntry;
 import arriba.fix.disruptor.FixMessageEntryFactory;
+import arriba.fix.disruptor.FixMessageToRingBufferEntryAdapter;
 import arriba.fix.disruptor.SerializedFixMessageToRingBufferEntryAdapter;
 import arriba.fix.disruptor.SessionNotifyingFixMessageEntryBatchHandler;
+import arriba.fix.messages.FixMessage;
+import arriba.fix.messages.FixMessageFactory;
 import arriba.fix.netty.FixMessageFrameDecoder;
+import arriba.fix.netty.InMemoryChannelRepository;
 import arriba.fix.netty.SerializedFixMessageHandler;
 import arriba.fix.session.AlwaysResolvingSessionResolver;
 import arriba.fix.session.SessionResolver;
@@ -30,25 +39,58 @@ import com.lmax.disruptor.WaitStrategy;
 public class FixServer {
 
     public void start() {
+        final Sender<ChannelBuffer> inboundRingBufferSender = this.createInboundFixMessageRingBuffer();
+        final Sender<FixMessage> outboundRingBufferSender = this.createOutboundFixMessageRingBuffer();
+
+        // TODO Use the outboundRingBufferSender to send FIX messages.
+
+        final ServerBootstrap bootstrap = this.server(inboundRingBufferSender);
+        bootstrap.bind(new InetSocketAddress(8080));
+    }
+
+    private Sender<ChannelBuffer> createInboundFixMessageRingBuffer() {
         final RingBuffer<FixMessageEntry> ringBuffer = new RingBuffer<FixMessageEntry>(new FixMessageEntryFactory(),
                 1024 * 32,
                 ClaimStrategy.Option.SINGLE_THREADED,
                 WaitStrategy.Option.BUSY_SPIN);
 
-        final ConsumerBarrier<FixMessageEntry> consumerBarrier = ringBuffer.createConsumerBarrier();
-        final Consumer consumer = new BatchConsumer<FixMessageEntry>(consumerBarrier,
+        final ConsumerBarrier<FixMessageEntry> deserializationConsumerBarrier = ringBuffer.createConsumerBarrier();
+        final Consumer deserializingConsumer = new BatchConsumer<FixMessageEntry>(deserializationConsumerBarrier,
+                new DeserializingFixMessageEntryBatchHandler(
+                        new FixMessageBuilder<ArrayFixChunk>(new ArrayFixChunkBuilder(),
+                                new ArrayFixChunkBuilder(), new ArrayFixChunkBuilder(),
+                                new FixMessageFactory())));
+
+        final ConsumerBarrier<FixMessageEntry> sessionNotificationConsumerBarrier = ringBuffer.createConsumerBarrier(deserializingConsumer);
+        final Consumer sessionNotifyingConsumer = new BatchConsumer<FixMessageEntry>(sessionNotificationConsumerBarrier,
                 new SessionNotifyingFixMessageEntryBatchHandler(this.sessionResolver()));
 
-        // FIXME Modify consumers to reflect introduction of deserializing consumer handler.
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(deserializingConsumer);
+        executorService.submit(sessionNotifyingConsumer);
 
-        final ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.submit(consumer);
+        final ProducerBarrier<FixMessageEntry> inboundProducerBarrier = ringBuffer.createProducerBarrier(deserializingConsumer, sessionNotifyingConsumer);
+        return new RingBufferSender<ChannelBuffer, FixMessageEntry>(inboundProducerBarrier,
+                new SerializedFixMessageToRingBufferEntryAdapter());
+    }
 
-        final ProducerBarrier<FixMessageEntry> producerBarrier = ringBuffer.createProducerBarrier(consumer);
+    private Sender<FixMessage> createOutboundFixMessageRingBuffer() {
+        final RingBuffer<FixMessageEntry> ringBuffer = new RingBuffer<FixMessageEntry>(new FixMessageEntryFactory(),
+                1024 * 32,
+                ClaimStrategy.Option.SINGLE_THREADED,
+                WaitStrategy.Option.BUSY_SPIN);
 
+        final ConsumerBarrier<FixMessageEntry> channelSubmissionConsumerBarrier = ringBuffer.createConsumerBarrier();
+        final Consumer channelSubmittingConsumer = new BatchConsumer<FixMessageEntry>(channelSubmissionConsumerBarrier,
+                new ChannelWritingFixMessageEntryBatchHandler(new InMemoryChannelRepository<String>()));
+        // TODO Populate the in-memory channel repository.
 
-        final ServerBootstrap bootstrap = this.server(producerBarrier);
-        bootstrap.bind(new InetSocketAddress(8080));
+        final ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(channelSubmittingConsumer);
+
+        final ProducerBarrier<FixMessageEntry> outboundProducerBarrier = ringBuffer.createProducerBarrier(channelSubmittingConsumer);
+        return new RingBufferSender<FixMessage, FixMessageEntry>(outboundProducerBarrier,
+                new FixMessageToRingBufferEntryAdapter());
     }
 
     private SessionResolver sessionResolver() {
@@ -57,13 +99,8 @@ public class FixServer {
         return new AlwaysResolvingSessionResolver();
     }
 
-    private ServerBootstrap server(final ProducerBarrier<FixMessageEntry> producerBarrier) {
-        final ChannelHandler deserializedFixMessageHandler = new SerializedFixMessageHandler(this.ringBufferSender(producerBarrier));
+    private ServerBootstrap server(final Sender<ChannelBuffer> inboundRingBufferSender) {
+        final ChannelHandler deserializedFixMessageHandler = new SerializedFixMessageHandler(inboundRingBufferSender);
         return FixServerBootstrap.create(new FixMessageFrameDecoder(), deserializedFixMessageHandler);
-    }
-
-    private Sender<ChannelBuffer> ringBufferSender(final ProducerBarrier<FixMessageEntry> producerBarrier) {
-        return new RingBufferSender<ChannelBuffer, FixMessageEntry>(producerBarrier,
-                new SerializedFixMessageToRingBufferEntryAdapter());
     }
 }
