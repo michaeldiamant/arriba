dfe572fca1c235479ace8fd8ab1d5f60418237ba
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-14 19:35:36 -0400

    Begin wiring in sample new order single message handler.

--- src/main/java/arriba/server/FixServer.java
+++ src/main/java/arriba/server/FixServer.java
@@ -1,6 +1,7 @@
 package arriba.server;
 
 import java.net.InetSocketAddress;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,6 +9,9 @@ import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.ChannelHandler;
 
+import arriba.common.Handler;
+import arriba.common.MapHandlerRepository;
+import arriba.common.PrintingHandler;
 import arriba.common.Sender;
 import arriba.fix.FixMessageBuilder;
 import arriba.fix.chunk.arrays.ArrayFixChunk;
@@ -21,13 +25,17 @@ import arriba.fix.disruptor.SerializedFixMessageToRingBufferEntryAdapter;
 import arriba.fix.disruptor.SessionNotifyingFixMessageEntryBatchHandler;
 import arriba.fix.messages.FixMessage;
 import arriba.fix.messages.FixMessageFactory;
+import arriba.fix.messages.NewOrderSingle;
 import arriba.fix.netty.FixMessageFrameDecoder;
 import arriba.fix.netty.InMemoryChannelRepository;
 import arriba.fix.netty.SerializedFixMessageHandler;
-import arriba.fix.session.AlwaysResolvingSessionResolver;
-import arriba.fix.session.SessionResolver;
+import arriba.fix.session.InMemorySessionResolver;
+import arriba.fix.session.Session;
+import arriba.fix.session.SessionId;
+import arriba.fix.session.SimpleSessionId;
 import arriba.senders.RingBufferSender;
 
+import com.google.common.collect.Maps;
 import com.lmax.disruptor.BatchConsumer;
 import com.lmax.disruptor.ClaimStrategy;
 import com.lmax.disruptor.Consumer;
@@ -37,6 +45,15 @@ import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.WaitStrategy;
 
 public class FixServer {
+
+    private final Map<SessionId, Session> sessionIdToSessions = Maps.newHashMap();
+
+    public FixServer() {
+        final SessionId sessionId = new SimpleSessionId("targetCompId");
+        final Map<String, Handler<NewOrderSingle>> messageIdentifierToHandlers = Maps.newHashMap();
+        messageIdentifierToHandlers.put("D", new PrintingHandler<NewOrderSingle>());
+        this.sessionIdToSessions.put(sessionId, new Session(sessionId, new MapHandlerRepository<String, NewOrderSingle>(messageIdentifierToHandlers)));
+    }
 
     public void start() {
         final Sender<ChannelBuffer> inboundRingBufferSender = this.createInboundFixMessageRingBuffer();
@@ -63,7 +80,7 @@ public class FixServer {
 
         final ConsumerBarrier<FixMessageEntry> sessionNotificationConsumerBarrier = ringBuffer.createConsumerBarrier(deserializingConsumer);
         final Consumer sessionNotifyingConsumer = new BatchConsumer<FixMessageEntry>(sessionNotificationConsumerBarrier,
-                new SessionNotifyingFixMessageEntryBatchHandler(this.sessionResolver()));
+                new SessionNotifyingFixMessageEntryBatchHandler(new InMemorySessionResolver(this.sessionIdToSessions)));
 
         final ExecutorService executorService = Executors.newFixedThreadPool(2);
         executorService.submit(deserializingConsumer);
@@ -91,12 +108,6 @@ public class FixServer {
         final ProducerBarrier<FixMessageEntry> outboundProducerBarrier = ringBuffer.createProducerBarrier(channelSubmittingConsumer);
         return new RingBufferSender<FixMessage, FixMessageEntry>(outboundProducerBarrier,
                 new FixMessageToRingBufferEntryAdapter());
-    }
-
-    private SessionResolver sessionResolver() {
-        // TODO Populate known sessions.
-        //        return new InMemorySessionResolver(new HashMap<SessionId, Session>());
-        return new AlwaysResolvingSessionResolver();
     }
 
     private ServerBootstrap server(final Sender<ChannelBuffer> inboundRingBufferSender) {
