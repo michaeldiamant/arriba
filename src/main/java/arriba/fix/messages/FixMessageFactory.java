677efd033c36d6a6f9a5e0b7260ab823f614cb97
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-20 21:20:20 -0400

    Add MarketDataSnapshotFullRefresh to FixMessageFactory.

--- src/main/java/arriba/fix/messages/FixMessageFactory.java
+++ src/main/java/arriba/fix/messages/FixMessageFactory.java
@@ -10,8 +10,11 @@ public final class FixMessageFactory {
             final FixChunk bodyChunk, final FixChunk trailerChunk,
             final Map<Integer, FixChunk[]> groupCountToGroupChunk) {
 
+        // TODO Replace literals with constants and try to move to switch statements (e.g. Java 7).
         if ("D".equals(messageType)) {
             return new NewOrderSingle(beginStringBytes, headerChunk, bodyChunk, trailerChunk, groupCountToGroupChunk);
+        } else if ("W".equals(messageType)) {
+            return new MarketDataSnapshotFullRefresh(beginStringBytes, headerChunk, bodyChunk, trailerChunk, groupCountToGroupChunk);
         } else {
             throw new IllegalArgumentException("Message type " + messageType + " does not map to a known FIX message.");
         }
