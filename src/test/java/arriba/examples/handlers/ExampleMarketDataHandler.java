60062a9ece74efd05c8e477217b255b8ec9643d5
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-20 20:26:05 -0400

    Introduce example handler of MarketDataSnapshotFullRefresh messages that creates NewOrderSingle messages for demonstration purposes.

---
+++ src/test/java/arriba/examples/handlers/ExampleMarketDataHandler.java
@@ -0,0 +1,68 @@
+package arriba.examples.handlers;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import arriba.common.Handler;
+import arriba.common.Sender;
+import arriba.fix.FixMessageBuilder;
+import arriba.fix.Tags;
+import arriba.fix.chunk.FixChunk;
+import arriba.fix.chunk.arrays.ArrayFixChunk;
+import arriba.fix.chunk.arrays.ArrayFixChunkBuilder;
+import arriba.fix.messages.FixMessage;
+import arriba.fix.messages.MarketDataSnapshotFullRefresh;
+
+public final class ExampleMarketDataHandler implements Handler<MarketDataSnapshotFullRefresh> {
+
+    private static final String LIMIT = "2";
+    private static final String BID = "0";
+    private static final String BUY = "1";
+    private static final String SELL = "2";
+    private static final String SENDING_TIME_FORMAT = "yyyyMMdd-HH:mm:ss";
+
+    private final AtomicInteger messageCount = new AtomicInteger();
+    private final Sender<FixMessage> sender;
+    private final FixMessageBuilder<ArrayFixChunk> fixMessageBuilder = new FixMessageBuilder<ArrayFixChunk>(new ArrayFixChunkBuilder(),
+            new ArrayFixChunkBuilder(), new ArrayFixChunkBuilder());
+
+    public ExampleMarketDataHandler(final Sender<FixMessage> sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void handle(final MarketDataSnapshotFullRefresh message) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(SENDING_TIME_FORMAT);
+
+        if (this.messageCount.getAndIncrement() % 2 == 0) {
+
+            this.fixMessageBuilder.addField(Tags.BODY_LENGTH, "34"); // TODO Compute body length.
+            this.fixMessageBuilder.addField(Tags.MESSAGE_TYPE, "D");
+            this.fixMessageBuilder.addField(Tags.SENDER_COMP_ID, message.getTargetCompId());
+            this.fixMessageBuilder.addField(Tags.TARGET_COMP_ID, message.getSenderCompId());
+            this.fixMessageBuilder.addField(Tags.SENDING_TIME, sdf.format(new Date()));
+
+            this.fixMessageBuilder.addField(Tags.CLIENT_ORDER_ID, String.valueOf(this.messageCount.get()));
+            this.fixMessageBuilder.addField(Tags.SYMBOL, message.getSymbol());
+
+
+            final FixChunk[] mdEntries = message.getGroup(Tags.NUMBER_MD_ENTRIES);
+            this.fixMessageBuilder.addField(Tags.PRICE, mdEntries[0].getValue(Tags.MD_ENTRY_PRICE));
+            this.fixMessageBuilder.addField(Tags.ORDER_TYPE, LIMIT);
+            if (mdEntries[0].getValue(Tags.MD_ENTRY_TYPE).equals(BID)) {
+                this.fixMessageBuilder.addField(Tags.SIDE, BUY);
+            } else {
+                this.fixMessageBuilder.addField(Tags.SIDE, SELL);
+            }
+
+            try {
+                this.sender.send(this.fixMessageBuilder.build());
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
