77d27627bebd219c4da477a238194e9bd394c0d3
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-21 11:15:29 -0400

    Introduce RepeatingGroups to associate number of repeating groups tag with associated repeating group tags.

---
+++ src/main/java/arriba/fix/RepeatingGroups.java
@@ -0,0 +1,31 @@
+package arriba.fix;
+
+import java.util.Arrays;
+
+public final class RepeatingGroups {
+
+    private static final int MAXIMUM_TAG = 1000;
+
+    public static final int[][] NUMBER_IN_GROUP_TAGS = new int[MAXIMUM_TAG][];
+
+    static {
+        populateNumberInGroupTags();
+        sortNumberInGroupTagValues();
+    }
+
+    private static void populateNumberInGroupTags() {
+        NUMBER_IN_GROUP_TAGS[Tags.NUMBER_MD_ENTRIES] = new int[] {
+                Tags.MD_ENTRY_PRICE,
+                Tags.MD_ENTRY_SIZE,
+                Tags.MD_ENTRY_TYPE
+        };
+    }
+
+    private static void sortNumberInGroupTagValues() {
+        for (final int[] repeatingGroupTags : NUMBER_IN_GROUP_TAGS) {
+            if (null != repeatingGroupTags) {
+                Arrays.sort(repeatingGroupTags);
+            }
+        }
+    }
+}
