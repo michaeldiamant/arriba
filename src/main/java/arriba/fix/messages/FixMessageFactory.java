b4ca7f6133bfd5a511d6ddf0b8f494fb80c4896c
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-08-20 20:45:58 -0400

    * Introduce fields package with BeginString class to enumerate constants for FIX fields. * Add byte[] member to FixMessage that represents the message BeginString. * Write BeginString when serializing FixMessage. * Reflect BeginString change throughout builders and other messages.

--- src/main/java/arriba/fix/messages/FixMessageFactory.java
+++ src/main/java/arriba/fix/messages/FixMessageFactory.java
@@ -6,10 +6,12 @@ import arriba.fix.chunk.FixChunk;
 
 public final class FixMessageFactory {
 
-    public static FixMessage create(final String messageType, final FixChunk headerChunk, final FixChunk bodyChunk,
-            final FixChunk trailerChunk, final Map<Integer, FixChunk[]> groupCountToGroupChunk) {
+    public static FixMessage create(final String messageType, final byte[] beginStringBytes, final FixChunk headerChunk,
+            final FixChunk bodyChunk, final FixChunk trailerChunk,
+            final Map<Integer, FixChunk[]> groupCountToGroupChunk) {
+
         if ("D".equals(messageType)) {
-            return new NewOrderSingle(headerChunk, bodyChunk, trailerChunk, groupCountToGroupChunk);
+            return new NewOrderSingle(beginStringBytes, headerChunk, bodyChunk, trailerChunk, groupCountToGroupChunk);
         } else {
             throw new IllegalArgumentException("Message type " + messageType + " does not map to a known FIX message.");
         }
