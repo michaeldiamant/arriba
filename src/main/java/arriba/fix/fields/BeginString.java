33288a81a9f1dc19a6790a38370abc432b90e47a
mdiamant <diamant.michael@gmail.com>
2011-09-04 18:36:23 -0400

    Mark BeginString class final.

--- src/main/java/arriba/fix/fields/BeginString.java
+++ src/main/java/arriba/fix/fields/BeginString.java
@@ -1,6 +1,6 @@
 package arriba.fix.fields;
 
-public class BeginString {
+public final class BeginString {
 
     public static byte[] FIXT11 = "FIXT.1.1".getBytes();
     public static byte[] FIX44 = "FIXT.4.4".getBytes();
