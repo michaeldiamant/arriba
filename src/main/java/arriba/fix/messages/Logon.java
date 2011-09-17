faa62e05ef5dcc61dddc1b09f602cd4dcfde3e6d
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-09-17 19:29:25 -0400

    Expose username and password field getters.

--- src/main/java/arriba/fix/messages/Logon.java
+++ src/main/java/arriba/fix/messages/Logon.java
@@ -2,6 +2,7 @@ package arriba.fix.messages;
 
 import java.util.Map;
 
+import arriba.fix.Tags;
 import arriba.fix.chunk.FixChunk;
 
 public final class Logon extends FixMessage {
@@ -9,5 +10,13 @@ public final class Logon extends FixMessage {
     public Logon(final byte[] beginStringBytes, final FixChunk headerChunk, final FixChunk bodyChunk, final FixChunk trailerChunk,
             final Map<Integer, FixChunk[]> groupCountToGroupChunk) {
         super(beginStringBytes, headerChunk, bodyChunk, trailerChunk, groupCountToGroupChunk);
+    }
+
+    public String getUsername() {
+        return this.getBodyValue(Tags.USERNAME);
+    }
+
+    public String getPassword() {
+        return this.getBodyValue(Tags.PASSWORD);
     }
 }
