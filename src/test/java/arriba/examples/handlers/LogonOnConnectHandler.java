49ee51142766bcb25c9b7ffa007c744787cc716c
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-09-17 18:10:22 -0400

    Provide FIX message handler to logon when TCP connection is established.

---
+++ src/test/java/arriba/examples/handlers/LogonOnConnectHandler.java
@@ -0,0 +1,72 @@
+package arriba.examples.handlers;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+import arriba.common.Sender;
+import arriba.fix.FixMessageBuilder;
+import arriba.fix.Tags;
+import arriba.fix.chunk.arrays.ArrayFixChunk;
+import arriba.fix.chunk.arrays.ArrayFixChunkBuilder;
+import arriba.fix.fields.BeginString;
+import arriba.fix.messages.FixMessage;
+import arriba.fix.netty.ChannelRepository;
+
+public class LogonOnConnectHandler extends SimpleChannelHandler {
+
+    private static final String SENDING_TIME_FORMAT = "yyyyMMdd-HH:mm:ss";
+
+    private final FixMessageBuilder<ArrayFixChunk> fixMessageBuilder =
+        new FixMessageBuilder<ArrayFixChunk>(new ArrayFixChunkBuilder(), new ArrayFixChunkBuilder(), new ArrayFixChunkBuilder());
+    private final AtomicInteger messageCount;
+    private final String senderCompId;
+    private final String targetCompId;
+    private final String username;
+    private final String password;
+    private final Sender<FixMessage> fixMessageSender;
+    private final ChannelRepository<String> channelRepository;
+
+    public LogonOnConnectHandler(final AtomicInteger messageCount,
+            final String senderCompId, final String targetCompId, final String username, final String password,
+            final Sender<FixMessage> fixMessageSender, final ChannelRepository<String> channelRepository) {
+        this.messageCount = messageCount;
+        this.senderCompId = senderCompId;
+        this.targetCompId = targetCompId;
+        this.username = username;
+        this.password = password;
+        this.fixMessageSender = fixMessageSender;
+        this.channelRepository = channelRepository;
+    }
+
+    @Override
+    public void channelConnected(final ChannelHandlerContext ctx, final ChannelStateEvent e) throws Exception {
+        final SimpleDateFormat sdf = new SimpleDateFormat(SENDING_TIME_FORMAT);
+
+        this.fixMessageBuilder.addField(Tags.MESSAGE_SEQUENCE_NUMBER, String.valueOf(this.messageCount.get()));
+        this.fixMessageBuilder.setMessageType("A");
+        this.fixMessageBuilder.setBeginStringBytes(BeginString.FIXT11);
+        this.fixMessageBuilder.addField(Tags.SENDER_COMP_ID, this.senderCompId);
+        this.fixMessageBuilder.addField(Tags.TARGET_COMP_ID, this.targetCompId);
+        this.fixMessageBuilder.addField(Tags.SENDING_TIME, sdf.format(new Date()));
+
+        this.fixMessageBuilder.addField(Tags.USERNAME, this.username);
+        this.fixMessageBuilder.addField(Tags.PASSWORD, this.password);
+
+        try {
+            this.fixMessageSender.send(this.fixMessageBuilder.build());
+
+            this.channelRepository.add(this.targetCompId, e.getChannel());
+        } catch (final IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        this.fixMessageBuilder.clear();
+    }
+
+}
