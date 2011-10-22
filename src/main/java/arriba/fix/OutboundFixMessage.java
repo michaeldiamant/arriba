4230bc812f5dae8711e706c1bac8d0cb8ff6bf59
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-10-21 21:59:14 -0400

    Introduce OutboundFixMessage class to wrap serialized FIX message and message destination (targetCompID).

---
+++ src/main/java/arriba/fix/OutboundFixMessage.java
@@ -0,0 +1,20 @@
+package arriba.fix;
+
+public final class OutboundFixMessage {
+
+    private final byte[] message;
+    private final String targetCompId;
+
+    public OutboundFixMessage(final byte[] message, final String targetCompId) {
+        this.message = message;
+        this.targetCompId = targetCompId;
+    }
+
+    public byte[] getMessage() {
+        return this.message;
+    }
+
+    public String getTargetCompId() {
+        return this.targetCompId;
+    }
+}
