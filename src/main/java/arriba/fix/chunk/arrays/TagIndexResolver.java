2b8e93374f527ee2a0ef5a307051678f09ce3535
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-10-22 17:31:46 -0400

    * Refactor TagIndexResolver hierarchy to promote array initialization and all operations from concrete TagIndexResolver to abstract class TagIndexResolver. * Expose two abstract methods on TagIndexResolver: (1) getRequiredTags() and (2) getOptionalTags(). * Update TagIndexResolver implementations to provide required and optional tags. * Introduce TagIndexResolverTest to exercise TagIndexResolver.

--- src/main/java/arriba/fix/chunk/arrays/TagIndexResolver.java
+++ src/main/java/arriba/fix/chunk/arrays/TagIndexResolver.java
@@ -1,16 +1,57 @@
 package arriba.fix.chunk.arrays;
 
+import java.util.Arrays;
+
+import com.google.common.primitives.Ints;
+
 public abstract class TagIndexResolver {
 
-    final protected static int INVALID_TAG_INDEX = -1;
+    private final static int INVALID_TAG_INDEX = -1;
 
-    public boolean isDefinedFor(final int tag) {
-        return tag <= this.getMaxTag() && this.getTagIndex(tag) != TagIndexResolver.INVALID_TAG_INDEX;
+    private final int[] tagIndexes;
+    private final int tagCount;
+    private final int maxTag;
+
+    public TagIndexResolver() {
+        final int[] requiredTags = this.getRequiredTags();
+        final int[] optionalTags = this.getOptionalTags();
+
+        if (0 == requiredTags.length) {
+            this.maxTag = Ints.max(optionalTags);
+        } else if (0 == optionalTags.length) {
+            this.maxTag = Ints.max(requiredTags);
+        } else {
+            this.maxTag = Ints.max(Ints.max(requiredTags), Ints.max(optionalTags));
+        }
+
+        this.tagIndexes = new int[this.maxTag + 1];
+        this.tagCount = requiredTags.length + optionalTags.length;
+
+        Arrays.fill(this.tagIndexes, INVALID_TAG_INDEX);
+        this.populateTagIndexes(requiredTags, 0);
+        this.populateTagIndexes(optionalTags, requiredTags.length);
     }
 
-    public abstract int getTagIndex(int tag);
+    private void populateTagIndexes(final int[] tags, int writeIndex) {
+        for (final int tag : tags) {
+            this.tagIndexes[tag] = writeIndex++;
+        }
+    }
 
-    public abstract int getTagCount();
+    public boolean isDefinedFor(final int tag) {
+        return tag <= this.maxTag &&
+                this.getTagIndex(tag) != TagIndexResolver.INVALID_TAG_INDEX;
+    }
 
-    protected abstract int getMaxTag();
+    public int getTagIndex(final int tag) {
+        return this.tagIndexes[tag];
+    }
+
+    public int getTagCount() {
+        return this.tagCount;
+    }
+
+    protected abstract int[] getRequiredTags();
+
+    protected abstract int[] getOptionalTags();
 }
