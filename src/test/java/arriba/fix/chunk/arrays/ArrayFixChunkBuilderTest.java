6fe4ba5a2546edab3f57d38a3cb46ee9ba7c2e58
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-10-22 11:41:43 -0400

    * Introduce FixChunkFieldCapturer to facilitate exercising FixChunkBuilders. * Introduce ArrayFixChunkBuilderTest to exercise ArrayFixChunkBuilder.

--- src/test/java/arriba/fix/chunk/arrays/ArrayFixChunkBuilderTest.java
+++ src/test/java/arriba/fix/chunk/arrays/ArrayFixChunkBuilderTest.java
@@ -1,12 +1,55 @@
 package arriba.fix.chunk.arrays;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import arriba.fix.Tags;
+import arriba.fix.chunk.FixChunk;
+import arriba.utils.FixChunkFieldCapturer;
 
 public class ArrayFixChunkBuilderTest {
 
-    @Test
-    public void testBuildingSingleFixChunk() {
-        // create test util to capture the fields from the outboundfixmessagebuildertest
+    private FixChunkFieldCapturer capturer;
+
+    @Before
+    public void before() {
+        this.capturer = new FixChunkFieldCapturer(new ArrayFixChunkBuilder());
     }
 
+    @Test
+    public void testBuildingSingleFixChunk() {
+        this.capturer.addField(Tags.SYMBOL, "EURUSD");
+        this.capturer.addField(Tags.ORDER_TYPE, "1");
+        this.capturer.addField(Tags.ORDER_QUANTITY, "5");
+        this.capturer.addField(Tags.SIDE, "1");
+        this.capturer.addField(Tags.CLIENT_ORDER_ID, "clOrdId");
+
+        final FixChunk fixChunk = this.capturer.build();
+
+        this.capturer.assertFieldsAreSet(fixChunk);
+    }
+
+    @Test
+    public void testBuildingTwoFixChunks() {
+        this.capturer.addField(Tags.SYMBOL, "EURUSD");
+        this.capturer.addField(Tags.ORDER_TYPE, "1");
+        this.capturer.addField(Tags.ORDER_QUANTITY, "5");
+        this.capturer.addField(Tags.SIDE, "1");
+        this.capturer.addField(Tags.CLIENT_ORDER_ID, "clOrdId");
+
+        this.capturer.build();
+        this.capturer.clear();
+
+        this.capturer.addField(Tags.MARKET_DEPTH, "1");
+        this.capturer.addField(Tags.SENDING_TIME, "now");
+        this.capturer.addField(Tags.TRANSACTION_TIME, "now");
+        this.capturer.addField(Tags.MD_ENTRY_PRICE, "1.245");
+        this.capturer.addField(Tags.MD_ENTRY_SIZE, "5");
+        this.capturer.addField(Tags.MD_ENTRY_TYPE, "1");
+        this.capturer.addField(Tags.MD_REQUEST_ID, "reqId1");
+
+        final FixChunk fixChunk = this.capturer.build();
+
+        this.capturer.assertFieldsAreSet(fixChunk);
+    }
 }
