710c27689bd0b051826c3a74d798d1919f790a39
Michael Diamant <diamant.michael@gmail.com>
2011-10-22 15:21:22 -0400

    Introduce AbstractTagIndexResolverTest to simplify TagIndexResolver implementation testing.

--- src/test/java/arriba/fix/chunk/arrays/StandardHeaderTagIndexResolverTest.java
+++ src/test/java/arriba/fix/chunk/arrays/StandardHeaderTagIndexResolverTest.java
@@ -1,38 +1,16 @@
 package arriba.fix.chunk.arrays;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-
 import arriba.fix.Tags;
-import arriba.fix.chunk.arrays.StandardHeaderTagIndexResolver;
-import arriba.fix.chunk.arrays.TagIndexResolver;
 
-public class StandardHeaderTagIndexResolverTest {
+public class StandardHeaderTagIndexResolverTest extends AbstractTagIndexResolverTest {
 
-    final TagIndexResolver resolver = new StandardHeaderTagIndexResolver();
-
-    @Test
-    public void testGetTagCount() {
-        assertThat(this.resolver.getTagCount(), is(Tags.getHeaders().length));
+    @Override
+    public TagIndexResolver getTagIndexResolver() {
+        return new StandardHeaderTagIndexResolver();
     }
 
-    @Test
-    public void testIsDefinedForWithDefinedTag() {
-        assertThat(this.resolver.isDefinedFor(Tags.BEGIN_STRING), is(true));
-    }
-
-    @Test
-    public void testIsDefinedForWithUndefinedTag() {
-        assertThat(this.resolver.isDefinedFor(Tags.SYMBOL), is(false));
-    }
-
-    @Test
-    public void testGetTagIndex() {
-        for (final int tag : Tags.getHeaders()) {
-            assertThat(this.resolver.getTagIndex(tag), is(not(TagIndexResolver.INVALID_TAG_INDEX)));
-        }
+    @Override
+    public int[] getAllTags() {
+        return Tags.getHeaders();
     }
 }
