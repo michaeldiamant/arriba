80716f74e61914755c821bde888b5574b400a359
Michael Diamant <diamant.michael@gmail.com>
2011-10-23 17:29:23 -0400

    Provide ArrayFixChunkBuilderSupplier to supply new ArrayFixChunkBuilder instances.

---
+++ src/main/java/arriba/fix/chunk/arrays/ArrayFixChunkBuilderSupplier.java
@@ -0,0 +1,29 @@
+package arriba.fix.chunk.arrays;
+
+import arriba.fix.chunk.FixChunkBuilder;
+import arriba.fix.chunk.FixChunkBuilderSupplier;
+import arriba.fix.tagindexresolvers.TagIndexResolverRepository;
+
+public final class ArrayFixChunkBuilderSupplier implements FixChunkBuilderSupplier {
+
+    private final TagIndexResolverRepository resolverRepository;
+
+    public ArrayFixChunkBuilderSupplier(final TagIndexResolverRepository resolverRepository) {
+        this.resolverRepository = resolverRepository;
+    }
+
+    @Override
+    public FixChunkBuilder getBodyBuilder(final byte[] messageType) {
+        return new ArrayFixChunkBuilder(this.resolverRepository.findBodyResolver(messageType));
+    }
+
+    @Override
+    public FixChunkBuilder getHeaderBuilder() {
+        return new ArrayFixChunkBuilder(this.resolverRepository.findHeaderResolver());
+    }
+
+    @Override
+    public FixChunkBuilder getTrailerBuilder() {
+        return new ArrayFixChunkBuilder(this.resolverRepository.findTrailerResolver());
+    }
+}
