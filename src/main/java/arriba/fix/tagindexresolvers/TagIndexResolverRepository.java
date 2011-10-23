b78cbec16623520c4947efbb73f04b00f2a5364f
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-10-23 17:27:40 -0400

    Introduce TagIndexResolverRepository interface to abstract away finding an appropriate TagIndexResolver instance.

---
+++ src/main/java/arriba/fix/tagindexresolvers/TagIndexResolverRepository.java
@@ -0,0 +1,10 @@
+package arriba.fix.tagindexresolvers;
+
+public interface TagIndexResolverRepository {
+
+    TagIndexResolver findBodyResolver(byte[] messageType);
+
+    TagIndexResolver findHeaderResolver();
+
+    TagIndexResolver findTrailerResolver();
+}
