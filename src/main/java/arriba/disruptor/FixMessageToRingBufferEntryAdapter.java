df32479a3b0cebbedb1c69a35964a33cc9224426
Michael Diamant <diamant.michael@gmail.com>
2011-10-29 22:06:05 -0400

    Format FixMessageToRingBufferEntryAdapter.

--- src/main/java/arriba/disruptor/FixMessageToRingBufferEntryAdapter.java
+++ src/main/java/arriba/disruptor/FixMessageToRingBufferEntryAdapter.java
@@ -2,7 +2,7 @@ package arriba.disruptor;
 
 import arriba.fix.inbound.InboundFixMessage;
 
-public final class FixMessageToRingBufferEntryAdapter implements MessageToDisruptorAdapter<InboundFixMessage, FixMessageEvent>{
+public final class FixMessageToRingBufferEntryAdapter implements MessageToDisruptorAdapter<InboundFixMessage, FixMessageEvent> {
 
     public void adapt(final InboundFixMessage inboundFixMessage, final FixMessageEvent entry) {
         entry.setFixMessage(inboundFixMessage);
