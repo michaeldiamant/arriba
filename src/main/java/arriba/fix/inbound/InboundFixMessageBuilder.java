d58aab626b6e90f8947d8c0ec91cea9b43ca0ce0
Michael Diamant <diamant.michael@gmail.com>
2011-10-29 22:33:45 -0400

    Remove deprecated methods from InboundFixMessageBuilder because there are no longer any usages of these methods.

--- src/main/java/arriba/fix/inbound/InboundFixMessageBuilder.java
+++ src/main/java/arriba/fix/inbound/InboundFixMessageBuilder.java
@@ -7,20 +7,16 @@ import java.util.Map;
 import arriba.fix.Tags;
 import arriba.fix.chunk.FixChunk;
 import arriba.fix.chunk.FixChunkBuilder;
-import arriba.fix.fields.BeginString;
 
 public final class InboundFixMessageBuilder {
 
-    private static final byte[] DEFAULT_BEGIN_STRING_BYTES = BeginString.FIXT11;
     private static final int[] HEADER_TAGS = Tags.getHeaders();
     private static final int[] TRAILER_TAGS = Tags.getTrailers();
 
-    private byte[] beginStringBytes = DEFAULT_BEGIN_STRING_BYTES; // TODO Why is this here?
     private final FixChunkBuilder headerChunkBuilder;
     private final FixChunkBuilder bodyChunkBuilder;
     private final FixChunkBuilder trailerChunkBuilder;
 
-    private byte[] messageType = null;
     private Map<Integer, FixChunk[]> repeatingGroupTagToRepeatingGroups;
 
     public InboundFixMessageBuilder(final FixChunkBuilder headerChunkBuilder, final FixChunkBuilder bodyChunkBuilder,
@@ -28,11 +24,6 @@ public final class InboundFixMessageBuilder {
         this.headerChunkBuilder = headerChunkBuilder;
         this.bodyChunkBuilder = bodyChunkBuilder;
         this.trailerChunkBuilder = trailerChunkBuilder;
-    }
-
-    @Deprecated
-    public InboundFixMessageBuilder addField(final int tag, final String value) {
-        return this.addField(tag, value.getBytes());
     }
 
     public InboundFixMessageBuilder addField(final int tag, final byte[] value) {
@@ -45,25 +36,6 @@ public final class InboundFixMessageBuilder {
         } else {
             this.bodyChunkBuilder.addField(tag, value);
         }
-
-        return this;
-    }
-
-    @Deprecated
-    public InboundFixMessageBuilder setBeginStringBytes(final byte[] beginStringBytes) {
-        this.beginStringBytes = beginStringBytes;
-
-        return this;
-    }
-
-    @Deprecated
-    public InboundFixMessageBuilder setMessageType(final String messageType) {
-        return this.setMessageType(messageType.getBytes());
-    }
-
-    @Deprecated
-    public InboundFixMessageBuilder setMessageType(final byte[] messageType) {
-        this.messageType = messageType;
 
         return this;
     }
@@ -86,12 +58,9 @@ public final class InboundFixMessageBuilder {
     }
 
     public void clear() {
-        this.beginStringBytes = DEFAULT_BEGIN_STRING_BYTES;
         this.headerChunkBuilder.clear();
         this.bodyChunkBuilder.clear();
         this.trailerChunkBuilder.clear();
         this.repeatingGroupTagToRepeatingGroups = null;
-
-        this.messageType = null;
     }
 }
