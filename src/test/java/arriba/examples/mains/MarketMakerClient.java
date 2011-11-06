fca6c8a8426f5b7ef68ab8660d12716be96067ef
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-11-05 23:40:25 -0400

    * Introduce ArribaWizard to simplify Arriba application creation. * Introduce DisruptorConfiguration to encapsulation configuration properties. * Refactor MarketMakerClient and MarketTakerClient to take advantage of ArribaWizard.

--- src/test/java/arriba/examples/mains/MarketMakerClient.java
+++ src/test/java/arriba/examples/mains/MarketMakerClient.java
@@ -2,7 +2,6 @@ package arriba.examples.mains;
 
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,36 +10,19 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelHandler;
 
-import arriba.common.Handler;
-import arriba.common.MapHandlerRepository;
 import arriba.common.PrintingHandler;
-import arriba.disruptor.DisruptorSender;
-import arriba.disruptor.inbound.DeserializingFixMessageEventHandler;
-import arriba.disruptor.inbound.InboundFixMessageEvent;
-import arriba.disruptor.inbound.InboundFixMessageEventFactory;
-import arriba.disruptor.inbound.SerializedFixMessageToDisruptorAdapter;
-import arriba.disruptor.inbound.SessionNotifyingInboundFixMessageEventHandler;
-import arriba.disruptor.outbound.OutboundFixMessageEvent;
-import arriba.disruptor.outbound.OutboundFixMessageEventFactory;
-import arriba.disruptor.outbound.TransportWritingFixMessageEventHandler;
+import arriba.common.Sender;
+import arriba.configuration.ArribaWizard;
+import arriba.configuration.DisruptorConfiguration;
 import arriba.examples.handlers.AuthenticatingLogonHandler;
 import arriba.examples.handlers.NewClientSessionHandler;
 import arriba.examples.handlers.SubscriptionManagingMarketDataRequestHandler;
 import arriba.examples.quotes.RandomQuoteSupplier;
 import arriba.examples.subscriptions.InMemorySubscriptionService;
 import arriba.examples.subscriptions.SubscriptionService;
-import arriba.fix.chunk.arrays.ArrayFixChunkBuilderSupplier;
-import arriba.fix.inbound.InboundFixMessageBuilder;
-import arriba.fix.inbound.InboundFixMessageFactory;
 import arriba.fix.inbound.NewOrderSingle;
-import arriba.fix.inbound.RepeatingGroupBuilder;
 import arriba.fix.outbound.OutboundFixMessage;
-import arriba.fix.session.InMemorySessionResolver;
-import arriba.fix.session.Session;
-import arriba.fix.session.SessionId;
-import arriba.fix.session.SimpleSessionId;
 import arriba.transport.InMemoryTransportRepository;
 import arriba.transport.TransportRepository;
 import arriba.transport.netty.FixMessageFrameDecoder;
@@ -48,105 +30,62 @@ import arriba.transport.netty.NettyTransportRepository;
 import arriba.transport.netty.SerializedFixMessageHandler;
 import arriba.transport.netty.bootstraps.FixServerBootstrap;
 
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.lmax.disruptor.ClaimStrategy;
-import com.lmax.disruptor.EventHandler;
-import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.WaitStrategy;
-import com.lmax.disruptor.wizard.DisruptorWizard;
 
 public class MarketMakerClient {
 
-    private final Map<SessionId, Session> sessionIdToSessions = Maps.newHashMap();
     private final AtomicInteger messageCount = new AtomicInteger();
-    private final TransportRepository<String, Channel> transportRepository =
-            new NettyTransportRepository<>(new InMemoryTransportRepository<String, Channel>());
     private final String senderCompId = "MM";
     private final String targetCompId = "MT";
     private final String expectedUsername = "tr8der";
     private final String expectedPassword = "liquidity";
     private final ExecutorService quotesExecutorService = Executors.newSingleThreadExecutor();
     private final SubscriptionService subscriptionService = new InMemorySubscriptionService();
-
-    // FIXME Need to rename existing FixMessageToRingBufferEntryAdapter to Inbound*."
-    // FIXME Introduce Outbound* versions of disruptor Inbound*.
-    private final DisruptorSender<OutboundFixMessage, OutboundFixMessageEvent> fixMessageSender = null;
-    private final DisruptorSender<ChannelBuffer, InboundFixMessageEvent> inboundRingBufferSender = new DisruptorSender<>(null,
-            new SerializedFixMessageToDisruptorAdapter());
-
     private final List<Channel> channels = new CopyOnWriteArrayList<Channel>();
 
-    public MarketMakerClient() {
-        final SessionId sessionId = new SimpleSessionId(this.targetCompId);
-        final Map<String, Handler<?>> messageIdentifierToHandlers = Maps.newHashMap();
-        messageIdentifierToHandlers.put("A",
-                new AuthenticatingLogonHandler(this.expectedUsername, this.expectedPassword,
-                        this.fixMessageSender, this.messageCount, this.channels, this.transportRepository));
-        messageIdentifierToHandlers.put("V",
-                new SubscriptionManagingMarketDataRequestHandler(this.subscriptionService));
-        messageIdentifierToHandlers.put("D",
-                new PrintingHandler<NewOrderSingle>());
-
-
-        this.sessionIdToSessions.put(sessionId, new Session(sessionId, new MapHandlerRepository(messageIdentifierToHandlers)));
-    }
+    public MarketMakerClient() {}
 
     public void start() {
-        // Quotes
-        final Runnable quoteSupplier = new RandomQuoteSupplier(this.subscriptionService, Sets.newHashSet("EURUSD"),
-                this.messageCount, this.senderCompId, this.fixMessageSender);
-        this.quotesExecutorService.submit(quoteSupplier);
+        final DisruptorConfiguration configuration = new DisruptorConfiguration(
+                512,
+                Executors.newCachedThreadPool(),
+                ClaimStrategy.Option.SINGLE_THREADED,
+                WaitStrategy.Option.YIELDING
+                );
+        final TransportRepository<String, Channel> repository = new NettyTransportRepository<>(new InMemoryTransportRepository<String, Channel>());
 
-        // Incoming
-        final DisruptorWizard<InboundFixMessageEvent> incomingDisruptor = new DisruptorWizard<InboundFixMessageEvent>(new InboundFixMessageEventFactory(), 1024, Executors.newCachedThreadPool() ,
-                ClaimStrategy.Option.SINGLE_THREADED, WaitStrategy.Option.YIELDING);
+        final ArribaWizard<Channel> wizard = new ArribaWizard<>(
+                configuration,
+                repository
+                );
 
-        incomingDisruptor.handleEventsWith(this.deserializingConsumer()).then(this.sessionNotifyingConsumer());
-        final RingBuffer<InboundFixMessageEvent> inboundRingBuffer = incomingDisruptor.start();
+        final Sender<ChannelBuffer> inboundSender = wizard.getInboundSender();
+        final Sender<OutboundFixMessage> outboundSender = wizard.getOutboundSender();
 
-        this.inboundRingBufferSender.setDisruptor(inboundRingBuffer);
+        wizard
+        .registerMessageHandler("A", new AuthenticatingLogonHandler(this.expectedUsername, this.expectedPassword, outboundSender, this.messageCount, this.channels, repository))
+        .registerMessageHandler("V", new SubscriptionManagingMarketDataRequestHandler(this.subscriptionService))
+        .registerMessageHandler("D", new PrintingHandler<NewOrderSingle>())
+
+        .registerTargetComponentIds(this.targetCompId);
+
+        this.initializeQuotes(outboundSender);
 
         final ServerBootstrap server = FixServerBootstrap.create(
                 new FixMessageFrameDecoder(),
                 new NewClientSessionHandler(this.channels),
-                this.deserializedFixMessageHandler()
+                new SerializedFixMessageHandler(inboundSender)
                 );
-
-
-        // Outgoing
-        final DisruptorWizard<OutboundFixMessageEvent> outgoingDisruptor = new DisruptorWizard<OutboundFixMessageEvent>(new OutboundFixMessageEventFactory(), 1024, Executors.newCachedThreadPool() ,
-                ClaimStrategy.Option.SINGLE_THREADED, WaitStrategy.Option.YIELDING);
-        outgoingDisruptor.handleEventsWith(this.channelWritingConsumer());
-
-        final RingBuffer<OutboundFixMessageEvent> outgoingRingBuffer = outgoingDisruptor.start();
-        this.fixMessageSender.setDisruptor(outgoingRingBuffer);
 
         server.bind(new InetSocketAddress("localhost", 8080));
     }
 
-    private InboundFixMessageBuilder inboundFixMessageBuilder() {
-        return new InboundFixMessageBuilder(
-                new ArrayFixChunkBuilderSupplier(null), // FIXME
-                new InboundFixMessageFactory()
-                );
-    }
-
-    private EventHandler<OutboundFixMessageEvent> channelWritingConsumer() {
-        return new TransportWritingFixMessageEventHandler<Channel>(this.transportRepository);
-    }
-
-    private EventHandler<InboundFixMessageEvent> sessionNotifyingConsumer() {
-        return new SessionNotifyingInboundFixMessageEventHandler(new InMemorySessionResolver(this.sessionIdToSessions));
-    }
-
-    private EventHandler<InboundFixMessageEvent> deserializingConsumer() {
-        return new DeserializingFixMessageEventHandler(this.inboundFixMessageBuilder(),
-                new RepeatingGroupBuilder(null)); // FIXME
-    }
-
-    private ChannelHandler deserializedFixMessageHandler() {
-        return new SerializedFixMessageHandler(this.inboundRingBufferSender);
+    private void initializeQuotes(final Sender<OutboundFixMessage> outboundSender) {
+        final Runnable quoteSupplier = new RandomQuoteSupplier(this.subscriptionService, Sets.newHashSet("EURUSD"),
+                this.messageCount, this.senderCompId, outboundSender);
+        this.quotesExecutorService.submit(quoteSupplier);
     }
 
     public static void main(final String[] args) {
--- src/test/java/arriba/examples/mains/MarketTakerClient.java
+++ src/test/java/arriba/examples/mains/MarketTakerClient.java
@@ -1,40 +1,21 @@
 package arriba.examples.mains;
 
 import java.net.InetSocketAddress;
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelHandler;
 
-import arriba.common.Handler;
-import arriba.common.MapHandlerRepository;
-import arriba.disruptor.DisruptorSender;
-import arriba.disruptor.inbound.DeserializingFixMessageEventHandler;
-import arriba.disruptor.inbound.InboundFixMessageEvent;
-import arriba.disruptor.inbound.InboundFixMessageEventFactory;
-import arriba.disruptor.inbound.SerializedFixMessageToDisruptorAdapter;
-import arriba.disruptor.inbound.SessionNotifyingInboundFixMessageEventHandler;
-import arriba.disruptor.outbound.OutboundFixMessageEvent;
-import arriba.disruptor.outbound.OutboundFixMessageEventFactory;
-import arriba.disruptor.outbound.TransportWritingFixMessageEventHandler;
+import arriba.common.Sender;
+import arriba.configuration.ArribaWizard;
+import arriba.configuration.DisruptorConfiguration;
 import arriba.examples.handlers.LogonOnConnectApplication;
 import arriba.examples.handlers.NewOrderGeneratingMarketDataHandler;
 import arriba.examples.handlers.SubscriptionRequestingLogonHandler;
-import arriba.fix.chunk.arrays.ArrayFixChunkBuilderSupplier;
-import arriba.fix.inbound.InboundFixMessageBuilder;
-import arriba.fix.inbound.InboundFixMessageFactory;
-import arriba.fix.inbound.RepeatingGroupBuilder;
 import arriba.fix.outbound.OutboundFixMessage;
-import arriba.fix.session.InMemorySessionResolver;
-import arriba.fix.session.Session;
-import arriba.fix.session.SessionId;
-import arriba.fix.session.SimpleSessionId;
 import arriba.transport.InMemoryTransportRepository;
-import arriba.transport.TransportConnectHandler;
 import arriba.transport.TransportRepository;
 import arriba.transport.netty.FixMessageFrameDecoder;
 import arriba.transport.netty.NettyConnectHandlerAdapter;
@@ -42,101 +23,50 @@ import arriba.transport.netty.NettyTransportRepository;
 import arriba.transport.netty.SerializedFixMessageHandler;
 import arriba.transport.netty.bootstraps.FixClientBootstrap;
 
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.lmax.disruptor.ClaimStrategy;
-import com.lmax.disruptor.EventHandler;
-import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.WaitStrategy;
-import com.lmax.disruptor.wizard.DisruptorWizard;
 
 public class MarketTakerClient {
 
-    private final Map<SessionId, Session> sessionIdToSessions = Maps.newHashMap();
     private final AtomicInteger messageCount = new AtomicInteger();
-    private final TransportRepository<String, Channel> transportRepository =
-            new NettyTransportRepository<String>(new InMemoryTransportRepository<String, Channel>());
     private final String senderCompId = "MT";
     private final String targetCompId = "MM";
     private final String username = "tr8der";
     private final String password = "liquidity";
 
-    private final DisruptorSender<OutboundFixMessage, OutboundFixMessageEvent> fixMessageSender = null;
-    private final DisruptorSender<ChannelBuffer, InboundFixMessageEvent> inboundRingBufferSender = new DisruptorSender<ChannelBuffer, InboundFixMessageEvent>(null,
-            new SerializedFixMessageToDisruptorAdapter());
-
-    public MarketTakerClient() {
-        final SessionId sessionId = new SimpleSessionId(this.targetCompId);
-        final Map<String, Handler<?>> messageIdentifierToHandlers = Maps.newHashMap();
-        messageIdentifierToHandlers.put("A",
-                new SubscriptionRequestingLogonHandler(Sets.newHashSet("EURUSD"), this.fixMessageSender, this.messageCount));
-        messageIdentifierToHandlers.put("W",
-                new NewOrderGeneratingMarketDataHandler(this.fixMessageSender, this.messageCount));
-
-        this.sessionIdToSessions.put(sessionId, new Session(sessionId, new MapHandlerRepository(messageIdentifierToHandlers)));
-    }
+    public MarketTakerClient() {}
 
     public void start() {
-        // Incoming
-        final DisruptorWizard<InboundFixMessageEvent> incomingDisruptor = new DisruptorWizard<InboundFixMessageEvent>(new InboundFixMessageEventFactory(), 1024, Executors.newCachedThreadPool() ,
-                ClaimStrategy.Option.SINGLE_THREADED, WaitStrategy.Option.YIELDING);
+        final DisruptorConfiguration configuration = new DisruptorConfiguration(
+                512,
+                Executors.newCachedThreadPool(),
+                ClaimStrategy.Option.SINGLE_THREADED,
+                WaitStrategy.Option.YIELDING
+                );
+        final TransportRepository<String, Channel> repository = new NettyTransportRepository<>(new InMemoryTransportRepository<String, Channel>());
 
-        incomingDisruptor.handleEventsWith(this.deserializingConsumer()).then(this.sessionNotifyingConsumer());
-        final RingBuffer<InboundFixMessageEvent> inboundRingBuffer = incomingDisruptor.start();
+        final ArribaWizard<Channel> wizard = new ArribaWizard<>(
+                configuration,
+                repository
+                );
 
-        this.inboundRingBufferSender.setDisruptor(inboundRingBuffer); // FIXME Major hack
+        final Sender<ChannelBuffer> inboundSender = wizard.getInboundSender();
+        final Sender<OutboundFixMessage> outboundSender = wizard.getOutboundSender();
+
+        wizard
+        .registerMessageHandler("A", new SubscriptionRequestingLogonHandler(Sets.newHashSet("EURUSD"), outboundSender, this.messageCount))
+        .registerMessageHandler("W", new NewOrderGeneratingMarketDataHandler(outboundSender, this.messageCount))
+
+        .registerTargetComponentIds(this.targetCompId);
 
         final ClientBootstrap client = FixClientBootstrap.create(
                 new FixMessageFrameDecoder(),
-                new NettyConnectHandlerAdapter(this.logonOnConnectApplication()),
-                this.deserializedFixMessageHandler()
+                new NettyConnectHandlerAdapter(new LogonOnConnectApplication<Channel>(this.messageCount, this.senderCompId, this.targetCompId, this.username, this.password, outboundSender, repository)),
+                new SerializedFixMessageHandler(inboundSender)
                 );
-
-
-        // Outgoing
-        final DisruptorWizard<OutboundFixMessageEvent> outgoingDisruptor = new DisruptorWizard<OutboundFixMessageEvent>(new OutboundFixMessageEventFactory(), 1024, Executors.newCachedThreadPool() ,
-                ClaimStrategy.Option.SINGLE_THREADED, WaitStrategy.Option.YIELDING);
-        outgoingDisruptor.handleEventsWith(this.channelWritingConsumer());
-
-        final RingBuffer<OutboundFixMessageEvent> outgoingRingBuffer = outgoingDisruptor.start();
-        this.fixMessageSender.setDisruptor(outgoingRingBuffer);  // FIXME This is a major hack.
 
         client.connect(new InetSocketAddress("localhost", 8080));
-
-        try {
-            Thread.sleep(1000 * 60 * 1);
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private InboundFixMessageBuilder inboundFixMessageBuilder() {
-        return new InboundFixMessageBuilder(
-                new ArrayFixChunkBuilderSupplier(null), // FIXME
-                new InboundFixMessageFactory()
-                );
-    }
-
-    private EventHandler<OutboundFixMessageEvent> channelWritingConsumer() {
-        return new TransportWritingFixMessageEventHandler<Channel>(this.transportRepository);
-    }
-
-    private EventHandler<InboundFixMessageEvent> sessionNotifyingConsumer() {
-        return new SessionNotifyingInboundFixMessageEventHandler(new InMemorySessionResolver(this.sessionIdToSessions));
-    }
-
-    private EventHandler<InboundFixMessageEvent> deserializingConsumer() {
-        return new DeserializingFixMessageEventHandler(this.inboundFixMessageBuilder(),
-                new RepeatingGroupBuilder(null)); // FIXME
-    }
-
-    private ChannelHandler deserializedFixMessageHandler() {
-        return new SerializedFixMessageHandler(this.inboundRingBufferSender);
-    }
-
-    private TransportConnectHandler logonOnConnectApplication() {
-        return new LogonOnConnectApplication(this.messageCount, this.senderCompId, this.targetCompId, this.username, this.password,
-                this.fixMessageSender, this.transportRepository);
     }
 
     public static void main(final String[] args) {
