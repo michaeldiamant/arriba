4567d59e1f3796965b8a51ee3d312c1f9f633ef2
Michael Diamant <diamant.michael@gmail.com>
2011-11-06 16:51:28 -0500

    Begin RepeatingGroupBuilderTest revitalization.  Tests still do not pass, so they remain ignored.

--- src/test/java/arriba/fix/inbound/RepeatingGroupBuilderTest.java
+++ src/test/java/arriba/fix/inbound/RepeatingGroupBuilderTest.java
@@ -1,53 +1,55 @@
-//package arriba.fix.inbound;
-//
-//import java.util.Map;
-//
-//import junit.framework.Assert;
-//
-//import org.junit.Ignore;
-//import org.junit.Test;
-//
-//import arriba.fix.Tags;
-//import arriba.fix.chunk.FixChunk;
-//
-//// FIXME Update tests.
-//
-//@Ignore
-//public class RepeatingGroupBuilderTest {
-//
-//    @Test
-//    public void testRepeatingGroupBuilding() {
-//        final RepeatingGroupBuilder builder = new RepeatingGroupBuilder();
-//
-//        builder.setNumberOfRepeatingGroups(2);
-//        builder.setNumberOfRepeatingGroupsTag(Tags.NUMBER_MD_ENTRIES);
-//
-//        builder.addField(Tags.MD_ENTRY_PRICE, "1.3455");
-//        builder.addField(Tags.MD_ENTRY_SIZE, "2");
-//        builder.addField(Tags.MD_ENTRY_TYPE, "1");
-//
-//        builder.addField(Tags.MD_ENTRY_PRICE, "1.3555");
-//        builder.addField(Tags.MD_ENTRY_SIZE, "3");
-//        builder.addField(Tags.MD_ENTRY_TYPE, "2");
-//
-//        builder.setNumberOfRepeatingGroups(3);
-//        builder.setNumberOfRepeatingGroupsTag(Tags.NUMBER_RELATED_SYMBOLS);
-//
-//        builder.addField(Tags.SYMBOL, "EURUSD");
-//        builder.addField(Tags.SYMBOL, "USDJPY");
-//        builder.addField(Tags.SYMBOL, "EURAUD");
-//
-//        final Map<Integer, FixChunk[]> repeatingGroupNoTagToRepeatingChunks = builder.build();
-//        Assert.assertEquals(2, repeatingGroupNoTagToRepeatingChunks.size());
-//
-//        final FixChunk[] mdEntries = repeatingGroupNoTagToRepeatingChunks.get(Tags.NUMBER_MD_ENTRIES);
-//        Assert.assertEquals(2, mdEntries.length);
-//
-//        Assert.assertEquals("1", mdEntries[0].getValue(Tags.MD_ENTRY_TYPE));
-//
-//        final FixChunk[] symbols = repeatingGroupNoTagToRepeatingChunks.get(Tags.NUMBER_RELATED_SYMBOLS);
-//        Assert.assertEquals(3, symbols.length);
-//        Assert.assertEquals("USDJPY", symbols[1].getValue(Tags.SYMBOL));
-//    }
-//
-//}
+package arriba.fix.inbound;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import junit.framework.Assert;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import arriba.fix.Tags;
+import arriba.fix.chunk.FixChunk;
+import arriba.fix.chunk.arrays.ArrayFixChunkBuilderSupplier;
+import arriba.fix.tagindexresolvers.CanonicalTagIndexResolverRepository;
+
+@Ignore
+public class RepeatingGroupBuilderTest {
+
+    private final RepeatingGroupBuilder builder =
+            new RepeatingGroupBuilder(new ArrayFixChunkBuilderSupplier(new CanonicalTagIndexResolverRepository()));
+
+    @Test
+    public void testRepeatingGroupBuilding() {
+        final int numberMdEntries = 2;
+        final int numberRelatedSymbols = 3;
+        this.builder
+        .setNumberOfRepeatingGroupsField(Tags.NUMBER_MD_ENTRIES, numberMdEntries)
+
+        .addField(Tags.MD_ENTRY_PRICE, "1.3455".getBytes())
+        .addField(Tags.MD_ENTRY_SIZE, "2".getBytes())
+        .addField(Tags.MD_ENTRY_TYPE, "1".getBytes())
+
+        .addField(Tags.MD_ENTRY_PRICE, "1.3555".getBytes())
+        .addField(Tags.MD_ENTRY_SIZE, "3".getBytes())
+        .addField(Tags.MD_ENTRY_TYPE, "2".getBytes())
+
+        .setNumberOfRepeatingGroupsField(Tags.NUMBER_RELATED_SYMBOLS, numberRelatedSymbols)
+
+        .addField(Tags.SYMBOL, "EURUSD".getBytes())
+        .addField(Tags.SYMBOL, "USDJPY".getBytes())
+        .addField(Tags.SYMBOL, "EURAUD".getBytes());
+
+        final FixChunk[][] repeatingGroups = this.builder.build();
+
+        assertThat(repeatingGroups.length, is(2));
+
+        final FixChunk[] mdEntries = repeatingGroups[0];
+        Assert.assertEquals(numberMdEntries, mdEntries.length);
+
+        Assert.assertEquals("1", mdEntries[0].getValue(Tags.MD_ENTRY_TYPE));
+
+        final FixChunk[] symbols = repeatingGroups[1];
+        Assert.assertEquals(numberRelatedSymbols, symbols.length);
+        Assert.assertEquals("USDJPY", symbols[1].getValue(Tags.SYMBOL));
+    }
+}
