c6040287c7cd18d4a390b6e13d6ac5ff790d17d9
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-11-11 11:19:34 -0500

    * Remove setting of sequence number and sending time in RichOutboundFixMessageBuilder.  This is done just prior to message serialization and writing. * Remove SequenceNumberGenerator and its implementation.

--- src/main/java/arriba/fix/outbound/RichOutboundFixMessageBuilder.java
+++ src/main/java/arriba/fix/outbound/RichOutboundFixMessageBuilder.java
@@ -3,16 +3,13 @@ package arriba.fix.outbound;
 import arriba.fix.Tags;
 import arriba.fix.fields.MessageType;
 import arriba.fix.inbound.InboundFixMessage;
-import arriba.fix.sequencenumbers.SequenceNumberGenerator;
 
 public final class RichOutboundFixMessageBuilder {
 
     private final RawOutboundFixMessageBuilder rawBuilder;
-    private final SequenceNumberGenerator generator;
 
-    public RichOutboundFixMessageBuilder(final RawOutboundFixMessageBuilder rawBuilder, final SequenceNumberGenerator generator) {
+    public RichOutboundFixMessageBuilder(final RawOutboundFixMessageBuilder rawBuilder) {
         this.rawBuilder = rawBuilder;
-        this.generator = generator;
     }
 
     public RichOutboundFixMessageBuilder addStandardHeader(final MessageType messageType, final InboundFixMessage message) {
@@ -38,12 +35,6 @@ public final class RichOutboundFixMessageBuilder {
     }
 
     public OutboundFixMessage build() {
-        this.addUtcTimestamp(Tags.SENDING_TIME);
-
-        this.rawBuilder
-        .addField(Tags.MESSAGE_SEQUENCE_NUMBER, Long.toString(this.generator.get()));
-
-
         return this.rawBuilder.build();
     }
 }
