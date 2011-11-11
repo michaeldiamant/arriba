199a4798af77e656afb39beab9724aa1ac7ca0a6
Michael Diamant <diamant.michael@gmail.com>
2011-11-11 10:56:04 -0500

    Inject SessionResolver into TransportWritingFixMessageEventHandler to find session associated with outbound message.

--- src/main/java/arriba/disruptor/outbound/TransportWritingFixMessageEventHandler.java
+++ src/main/java/arriba/disruptor/outbound/TransportWritingFixMessageEventHandler.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 import arriba.fix.outbound.DateSupplier;
 import arriba.fix.outbound.OutboundFixMessage;
+import arriba.fix.session.Session;
+import arriba.fix.session.SessionId;
+import arriba.fix.session.SessionResolver;
 import arriba.transport.Transport;
 import arriba.transport.TransportRepository;
 
@@ -12,9 +15,12 @@ import com.lmax.disruptor.EventHandler;
 public final class TransportWritingFixMessageEventHandler<T> implements EventHandler<OutboundFixMessageEvent> {
 
     private final TransportRepository<String, T> transportRepository;
+    private final SessionResolver sessionResolver;
 
-    public TransportWritingFixMessageEventHandler(final TransportRepository<String, T> transportRepository) {
+    public TransportWritingFixMessageEventHandler(final TransportRepository<String, T> transportRepository,
+            final SessionResolver sessionResolver) {
         this.transportRepository = transportRepository;
+        this.sessionResolver = sessionResolver;
     }
 
     @Override
@@ -25,6 +31,9 @@ public final class TransportWritingFixMessageEventHandler<T> implements EventHan
         if (null == transport) {
             throw new IOException("");
         }
+
+        // TODO Can SessionId be cached?
+        final Session session = this.sessionResolver.resolve(new SessionId("", fixMessage.getTargetCompId()));
 
         // TODO Find session associated with message to get correct sequence number.
         final int messageSequenceNumber = 0;
