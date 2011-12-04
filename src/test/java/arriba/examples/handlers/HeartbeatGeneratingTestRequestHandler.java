66a22ea2b544afa53612e2ccdccb9351600776c7
Michael Diamant <diamant.michael@gmail.com>
2011-12-04 11:25:16 -0500

    * Introduce HeartbeatGeneratingTestRequestHandler to respond to test request messages with a heartbeat. * Register HeartbeatGeneratingTestRequestHandler in MarketTakerClient and MarketMakerClient.

---
+++ src/test/java/arriba/examples/handlers/HeartbeatGeneratingTestRequestHandler.java
@@ -0,0 +1,36 @@
+package arriba.examples.handlers;
+
+import java.io.IOException;
+
+import arriba.common.Handler;
+import arriba.common.Sender;
+import arriba.fix.Tags;
+import arriba.fix.fields.MessageType;
+import arriba.fix.inbound.TestRequest;
+import arriba.fix.outbound.OutboundFixMessage;
+import arriba.fix.outbound.RichOutboundFixMessageBuilder;
+
+public final class HeartbeatGeneratingTestRequestHandler implements Handler<TestRequest>{
+
+    private final RichOutboundFixMessageBuilder builder;
+    private final Sender<OutboundFixMessage> sender;
+
+    public HeartbeatGeneratingTestRequestHandler(final RichOutboundFixMessageBuilder builder, final Sender<OutboundFixMessage> sender) {
+        this.builder = builder;
+        this.sender = sender;
+    }
+
+    @Override
+    public void handle(final TestRequest message) {
+        final OutboundFixMessage heartbeat = this.builder
+                .addStandardHeader(MessageType.HEARTBEAT, message)
+                .addField(Tags.TEST_REQUEST_ID, message.getTestRequestId())
+                .build();
+
+        try {
+            this.sender.send(heartbeat);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
