5893f8e04ca7648407655c3ca86b8c5cd31642d4
Michael Diamant <diamant.michael@gmail.com>
2011-12-10 11:11:30 -0500

    * Provide CompIdToDisruptorAdapter to create outbound Disruptor events representing session disconnect. * Modify OutboundFixMessageEvent to maintain targetCompId property for disconnecting. * Make OutboundFixMessageToDisruptorAdapter implement CompIdToDisruptorAdapter to support producing session disconnect events.

--- src/main/java/arriba/disruptor/outbound/OutboundFixMessageEvent.java
+++ src/main/java/arriba/disruptor/outbound/OutboundFixMessageEvent.java
@@ -7,6 +7,7 @@ import com.lmax.disruptor.AbstractEvent;
 public final class OutboundFixMessageEvent extends AbstractEvent {
 
     private OutboundFixMessage message;
+    private String targetCompId;
 
     public OutboundFixMessageEvent() {}
 
@@ -16,5 +17,13 @@ public final class OutboundFixMessageEvent extends AbstractEvent {
 
     public void setFixMessage(final OutboundFixMessage message) {
         this.message = message;
+    }
+
+    public String getTargetCompId() {
+        return this.targetCompId;
+    }
+
+    public void setTargetCompId(final String targetCompId) {
+        this.targetCompId = targetCompId;
     }
 }
--- src/main/java/arriba/disruptor/outbound/OutboundFixMessageToDisruptorAdapter.java
+++ src/main/java/arriba/disruptor/outbound/OutboundFixMessageToDisruptorAdapter.java
@@ -1,12 +1,22 @@
 package arriba.disruptor.outbound;
 
+import arriba.disruptor.CompIdToDisruptorAdapter;
 import arriba.disruptor.MessageToDisruptorAdapter;
 import arriba.fix.outbound.OutboundFixMessage;
 
-public final class OutboundFixMessageToDisruptorAdapter implements MessageToDisruptorAdapter<OutboundFixMessage, OutboundFixMessageEvent> {
+public final class OutboundFixMessageToDisruptorAdapter implements
+MessageToDisruptorAdapter<OutboundFixMessage, OutboundFixMessageEvent>,
+CompIdToDisruptorAdapter<OutboundFixMessageEvent> {
 
     @Override
     public void adapt(final OutboundFixMessage message, final OutboundFixMessageEvent event) {
         event.setFixMessage(message);
+        event.setTargetCompId(null);
+    }
+
+    @Override
+    public void adapt(final String compId, final OutboundFixMessageEvent event) {
+        event.setFixMessage(null);
+        event.setTargetCompId(compId);
     }
 }
