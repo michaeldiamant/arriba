3a37497dc3756ec6bc6a7c2689eb1f5dceff1213
Michael Diamant <diamant.michael@gmail.com>
2011-12-10 11:42:29 -0500

    Extract interface from existing SessionMonitor and define implementation as ScheduledSessionMonitor.

---
+++ src/main/java/arriba/fix/session/ScheduledSessionMonitor.java
@@ -0,0 +1,116 @@
+package arriba.fix.session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import arriba.common.Sender;
+import arriba.fix.Tags;
+import arriba.fix.fields.BeginString;
+import arriba.fix.fields.MessageType;
+import arriba.fix.outbound.OutboundFixMessage;
+import arriba.fix.outbound.RichOutboundFixMessageBuilder;
+
+public final class ScheduledSessionMonitor implements SessionMonitor {
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final Sender<OutboundFixMessage> fixMessageSender;
+    private final RichOutboundFixMessageBuilder builder;
+    private final Map<Session, ScheduledFuture<?>> sessionToMonitorFuture = new HashMap<>();
+    private final Lock sessionToMonitorFutureLock = new ReentrantLock();
+    private final SessionDisconnector disconnector;
+
+    public ScheduledSessionMonitor(final Sender<OutboundFixMessage> fixMessageSender,
+            final RichOutboundFixMessageBuilder builder,
+            final SessionDisconnector disconnector) {
+        this.fixMessageSender = fixMessageSender;
+        this.builder = builder;
+        this.disconnector = disconnector;
+    }
+
+    // TODO How to know when to unregister a session
+
+    @Override
+    public void unmonitor(final Session session) {
+        this.sessionToMonitorFutureLock.lock();
+        try {
+            this.sessionToMonitorFuture.remove(session);
+        } finally {
+            this.sessionToMonitorFutureLock.unlock();
+        }
+    }
+
+    @Override
+    public void monitor(final Session session, final long heartbeatIntervalInMs) {
+        this.sessionToMonitorFutureLock.lock();
+        try {
+            if (!this.sessionToMonitorFuture.containsKey(session)) {
+                final Runnable monitorRunnable = new Runnable() {
+
+                    private boolean isHeartbeatRequired() {
+                        return System.currentTimeMillis() - session.getLastSentTimestamp() >= heartbeatIntervalInMs;
+                    }
+
+                    private boolean isTestRequestRequired() {
+                        return System.currentTimeMillis() - session.getLastReceivedTimestamp() >= heartbeatIntervalInMs;
+                    }
+
+                    private boolean isLogoutRequired() {
+                        // No messages received in 2+ heartbeat intervals indicates that the session did not respond to test request.
+                        return System.currentTimeMillis() - session.getLastReceivedTimestamp() >= 2 * heartbeatIntervalInMs;
+                    }
+
+                    private boolean hasTimedoutWaitingForLogoutResponse() {
+                        // No messages received in 3+ hearbeat intervals indicates that the session did not respond to logout request.
+                        return System.currentTimeMillis() - session.getLastReceivedTimestamp() >= 3 * heartbeatIntervalInMs;
+                    }
+
+                    @Override
+                    public void run() {
+                        if (this.hasTimedoutWaitingForLogoutResponse()) {
+                            ScheduledSessionMonitor.this.disconnector.disconnect(session.getTargetCompId());
+                        } else if (this.isLogoutRequired()) {
+                            this.sendLogout();
+                        } else if (this.isTestRequestRequired()) {
+                            this.sendTestRequest();
+                        } else if (this.isHeartbeatRequired()) {
+                            this.sendHeartbeat();
+                        }
+                    }
+
+                    private void sendLogout() {
+                        final OutboundFixMessage logout = ScheduledSessionMonitor.this.builder
+                                .addStandardHeader(MessageType.LOGOUT, BeginString.FIXT11.getValue(), session.getSenderCompId(), session.getTargetCompId())
+                                .build();
+                        ScheduledSessionMonitor.this.fixMessageSender.send(logout);
+                    }
+
+                    private void sendHeartbeat() {
+                        final OutboundFixMessage heartbeat = ScheduledSessionMonitor.this.builder
+                                .addStandardHeader(MessageType.HEARTBEAT, BeginString.FIXT11.getValue(), session.getSenderCompId(), session.getTargetCompId())
+                                .build();
+                        ScheduledSessionMonitor.this.fixMessageSender.send(heartbeat);
+                    }
+
+                    private void sendTestRequest() {
+                        final OutboundFixMessage testRequest = ScheduledSessionMonitor.this.builder
+                                .addStandardHeader(MessageType.TEST_REQUEST, BeginString.FIXT11.getValue(), session.getSenderCompId(), session.getTargetCompId())
+                                .addField(Tags.TEST_REQUEST_ID, Long.toString(System.currentTimeMillis()))
+                                .build();
+                        ScheduledSessionMonitor.this.fixMessageSender.send(testRequest);
+                    }
+                };
+
+                final ScheduledFuture<?> monitorFuture = this.executor.scheduleAtFixedRate(monitorRunnable, 0, heartbeatIntervalInMs, TimeUnit.MILLISECONDS);
+                this.sessionToMonitorFuture.put(session, monitorFuture);
+            }
+        } finally {
+            this.sessionToMonitorFutureLock.unlock();
+        }
+    }
+}
