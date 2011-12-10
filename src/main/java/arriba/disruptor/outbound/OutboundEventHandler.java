303723d7979d01876b0189d15b819fbafd3250e2
Michael Diamant <diamant.michael@gmail.com>
2011-12-10 16:53:02 -0500

    * Rename TransportWritingFixMessageEventHandler to OutboundEventHandler because it is deals with more than just FIX messages. * Refactor OutboundEventHandler to delegate to handlers for OutboundFixMessage and SessionId. * Provide TransportWritingFixMessageHandler to handle outbound FIX messages. * Provide DisconnectingSessionIdHandler to close session's transport and notify listeners.

---
+++ src/main/java/arriba/disruptor/outbound/OutboundEventHandler.java
@@ -0,0 +1,30 @@
+package arriba.disruptor.outbound;
+
+import arriba.common.Handler;
+import arriba.fix.outbound.OutboundFixMessage;
+import arriba.fix.session.SessionId;
+
+import com.lmax.disruptor.EventHandler;
+
+public final class OutboundEventHandler<T> implements EventHandler<OutboundEvent> {
+
+    private final Handler<OutboundFixMessage> fixMessageHandler;
+    private final Handler<SessionId> sessionIdHandler;
+
+    public OutboundEventHandler(final Handler<OutboundFixMessage> fixMessageHandler,
+            final Handler<SessionId> sessionIdHandler) {
+        this.fixMessageHandler = fixMessageHandler;
+        this.sessionIdHandler = sessionIdHandler;
+    }
+
+    @Override
+    public void onEvent(final OutboundEvent entry, final boolean endOfBatch) throws Exception {
+        if (null != entry.getFixMessage()) {
+            this.fixMessageHandler.handle(entry.getFixMessage());
+        }
+
+        if (null != entry.getSessionId()) {
+            this.sessionIdHandler.handle(entry.getSessionId());
+        }
+    }
+}
