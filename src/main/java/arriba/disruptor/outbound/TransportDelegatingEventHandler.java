1845404c97f426014c73dfdee56d817a5aa8262d
michaeldiamant <michaeldiamant@users.noreply.github.com>
2011-12-18 13:47:15 -0500

    * Revert renaming to TransportWritingFixMessageFunction from TransportWritingFixMessageHandler. * Add outbound message sequence number to OutboundEvent. * Set sequence number and serialized message in OutboundEvent in TransportWritingFixMessageHandler to support journaling in subsequent event handler.

--- src/main/java/arriba/disruptor/outbound/TransportDelegatingEventHandler.java
+++ src/main/java/arriba/disruptor/outbound/TransportDelegatingEventHandler.java
@@ -1,28 +1,25 @@
 package arriba.disruptor.outbound;
 
 import arriba.common.Handler;
-import arriba.fix.outbound.OutboundFixMessage;
 import arriba.fix.session.SessionId;
 
-import com.google.common.base.Function;
 import com.lmax.disruptor.EventHandler;
 
 public final class TransportDelegatingEventHandler<T> implements EventHandler<OutboundEvent> {
 
-    private final Function<OutboundFixMessage, byte[]> fixMessageFunc;
+    private final Handler<OutboundEvent> fixMessageHandler;
     private final Handler<SessionId> sessionIdHandler;
 
-    public TransportDelegatingEventHandler(final Function<OutboundFixMessage, byte[]> fixMessageHandler,
+    public TransportDelegatingEventHandler(final Handler<OutboundEvent> fixMessageHandler,
             final Handler<SessionId> sessionIdHandler) {
-        this.fixMessageFunc = fixMessageHandler;
+        this.fixMessageHandler = fixMessageHandler;
         this.sessionIdHandler = sessionIdHandler;
     }
 
     @Override
     public void onEvent(final OutboundEvent event, final boolean endOfBatch) throws Exception {
         if (null != event.getFixMessage()) {
-            final byte[] messageBytes = this.fixMessageFunc.apply(event.getFixMessage());
-            event.setSerializedFixMessage(messageBytes);
+            this.fixMessageHandler.handle(event);
         }
 
         if (null != event.getSessionId()) {
