0de8d6ed4ff777bf472b70e68fdbc5762744bce2
Michael Diamant <diamant.michael@gmail.com>
2012-01-01 15:44:22 -0500

    Move inbound deserialization logic into separate class so it can be used in other parts of the system.

--- arriba-core/src/main/java/arriba/disruptor/inbound/DeserializingFixMessageEventHandler.java
+++ arriba-core/src/main/java/arriba/disruptor/inbound/DeserializingFixMessageEventHandler.java
@@ -1,40 +1,21 @@
 package arriba.disruptor.inbound;
 
-import java.util.Arrays;
-
 import org.jboss.netty.buffer.ChannelBuffer;
 
-import arriba.fix.Fields;
-import arriba.fix.RepeatingGroups;
-import arriba.fix.Tags;
 import arriba.fix.inbound.InboundFixMessage;
 import arriba.fix.inbound.InboundFixMessageBuilder;
 import arriba.fix.inbound.RepeatingGroupBuilder;
+import arriba.fix.inbound.deserializers.InboundFixMessageDeserializer;
 
 import com.lmax.disruptor.EventHandler;
 
 public final class DeserializingFixMessageEventHandler implements EventHandler<InboundEvent> {
 
-    private static final byte[] CHECKSUM_BYTES = Tags.toByteArray(Tags.CHECKSUM);
-
-    private final InboundFixMessageBuilder inboundFixMessageBuilder;
-    private final RepeatingGroupBuilder repeatingGroupBuilder;
-
-    private byte nextFlagByte;
-    private int nextFlagIndex;
-    private boolean hasFoundFinalDelimiter;
-    private int lastDeserializedTag;
-
-    private ParsingState parsingState;
-    private int[] repeatingGroupTags;
-    private boolean hasFoundNumberOfRepeatingGroupsTag;
+    private final InboundFixMessageDeserializer deserializer;
 
     public DeserializingFixMessageEventHandler(final InboundFixMessageBuilder inboundFixMessageBuilder,
             final RepeatingGroupBuilder repeatingGroupBuilder) {
-        this.inboundFixMessageBuilder = inboundFixMessageBuilder;
-        this.repeatingGroupBuilder = repeatingGroupBuilder;
-
-        this.reset();
+        this.deserializer = new InboundFixMessageDeserializer(inboundFixMessageBuilder, repeatingGroupBuilder);
     }
 
     @Override
@@ -43,108 +24,10 @@ public final class DeserializingFixMessageEventHandler implements EventHandler<I
         final InboundFixMessage[] messages = new InboundFixMessage[serializedMessages.length];
 
         for (int messageIndex = 0; messageIndex < serializedMessages.length; messageIndex++) {
-            this.parsingState = ParsingState.NON_REPEATING_GROUP;
-            messages[messageIndex] = this.deserializeFixMessage(serializedMessages[messageIndex]);
+            messages[messageIndex] = this.deserializer.deserialize(serializedMessages[messageIndex]);
         }
 
         event.setMessages(messages);
     }
 
-    private InboundFixMessage deserializeFixMessage(final ChannelBuffer buffer) throws DeserializationException {
-        while ((this.nextFlagIndex = buffer.bytesBefore(this.nextFlagByte)) != -1) {
-            final ChannelBuffer nextValueBuffer = buffer.readBytes(this.nextFlagIndex);
-            buffer.readerIndex(buffer.readerIndex() + 1);
-
-            if (Fields.EQUAL_SIGN == this.nextFlagByte) {
-                this.nextFlagByte = Fields.DELIMITER;
-
-                final byte[] tag = nextValueBuffer.array();
-                if (Arrays.equals(CHECKSUM_BYTES, tag)) {
-                    this.hasFoundFinalDelimiter = true;
-                }
-
-                // TODO Make lookup table.
-                this.lastDeserializedTag = Integer.parseInt(new String(tag));
-
-                switch (this.parsingState) {
-                case REPEATING_GROUP:
-                    if (Arrays.binarySearch(this.repeatingGroupTags, this.lastDeserializedTag) < 0) {
-                        if (this.hasFoundNumberOfRepeatingGroupsTag()) {
-                            this.handleNewRepeatingGroup();
-                        } else {
-                            this.parsingState = ParsingState.NON_REPEATING_GROUP;
-                        }
-                    }
-
-                    break;
-                case NON_REPEATING_GROUP:
-                    if (this.hasFoundNumberOfRepeatingGroupsTag()) {
-                        this.handleNewRepeatingGroup();
-                    }
-
-                    break;
-                }
-            } else if (Fields.DELIMITER == this.nextFlagByte) {
-                this.nextFlagByte = Fields.EQUAL_SIGN;
-
-                final byte[] value = nextValueBuffer.array();
-
-                switch (this.parsingState) {
-                case REPEATING_GROUP:
-                    if (this.hasFoundNumberOfRepeatingGroupsTag) {
-                        this.hasFoundNumberOfRepeatingGroupsTag = false;
-
-                        this.repeatingGroupBuilder.setNumberOfRepeatingGroupsTag(this.lastDeserializedTag);
-                    } else {
-                        this.repeatingGroupBuilder.addField(this.lastDeserializedTag, value);
-
-                        // TODO Hook in repeating groups into inbound FIX message builder.
-                    }
-
-                    break;
-                case NON_REPEATING_GROUP:
-                    this.inboundFixMessageBuilder.addField(this.lastDeserializedTag, value);
-                    if (this.hasFoundFinalDelimiter) {
-                        this.hasFoundFinalDelimiter = false;
-
-                        final InboundFixMessage inboundFixMessage =
-                                this.inboundFixMessageBuilder.build(this.repeatingGroupBuilder.build(),
-                                        this.repeatingGroupBuilder.getNumberOfRepeatingGroupTags());
-                        this.reset();
-
-                        return inboundFixMessage;
-                    }
-
-                    break;
-                }
-            }
-        }
-
-        // TODO Consider how to better handle this situation.
-        throw new DeserializationException("Unable to deserialize FIX message.");
-    }
-
-    private void handleNewRepeatingGroup() {
-        this.repeatingGroupTags = RepeatingGroups.NUMBER_IN_GROUP_TAGS[this.lastDeserializedTag];
-        this.parsingState = ParsingState.REPEATING_GROUP;
-        this.hasFoundNumberOfRepeatingGroupsTag = true;
-    }
-
-    private boolean hasFoundNumberOfRepeatingGroupsTag() {
-        return null != RepeatingGroups.NUMBER_IN_GROUP_TAGS[this.lastDeserializedTag];
-    }
-
-    private void reset() {
-        this.nextFlagIndex = -1;
-        this.nextFlagByte = Fields.EQUAL_SIGN;
-        this.hasFoundFinalDelimiter = false;
-        this.lastDeserializedTag = -1;
-        this.repeatingGroupTags = null;
-        this.inboundFixMessageBuilder.clear();
-        this.repeatingGroupBuilder.clear();
-    }
-
-    private static enum ParsingState {
-        REPEATING_GROUP, NON_REPEATING_GROUP
-    }
 }
---
+++ arriba-core/src/main/java/arriba/fix/inbound/deserializers/InboundFixMessageDeserializer.java
@@ -0,0 +1,137 @@
+package arriba.fix.inbound.deserializers;
+
+import java.util.Arrays;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+
+import arriba.disruptor.inbound.DeserializationException;
+import arriba.fix.Fields;
+import arriba.fix.RepeatingGroups;
+import arriba.fix.Tags;
+import arriba.fix.inbound.InboundFixMessage;
+import arriba.fix.inbound.InboundFixMessageBuilder;
+import arriba.fix.inbound.RepeatingGroupBuilder;
+
+public final class InboundFixMessageDeserializer {
+
+    private static final byte[] CHECKSUM_BYTES = Tags.toByteArray(Tags.CHECKSUM);
+
+    private final InboundFixMessageBuilder inboundFixMessageBuilder;
+    private final RepeatingGroupBuilder repeatingGroupBuilder;
+
+    private byte nextFlagByte;
+    private int nextFlagIndex;
+    private boolean hasFoundFinalDelimiter;
+    private int lastDeserializedTag;
+
+    private ParsingState parsingState;
+    private int[] repeatingGroupTags;
+    private boolean hasFoundNumberOfRepeatingGroupsTag;
+
+    public InboundFixMessageDeserializer(final InboundFixMessageBuilder inboundFixMessageBuilder,
+            final RepeatingGroupBuilder repeatingGroupBuilder) {
+        this.inboundFixMessageBuilder = inboundFixMessageBuilder;
+        this.repeatingGroupBuilder = repeatingGroupBuilder;
+
+        this.reset();
+    }
+
+
+    public InboundFixMessage deserialize(final ChannelBuffer buffer) {
+        while ((this.nextFlagIndex = buffer.bytesBefore(this.nextFlagByte)) != -1) {
+            final ChannelBuffer nextValueBuffer = buffer.readBytes(this.nextFlagIndex);
+            buffer.readerIndex(buffer.readerIndex() + 1);
+
+            if (Fields.EQUAL_SIGN == this.nextFlagByte) {
+                this.nextFlagByte = Fields.DELIMITER;
+
+                final byte[] tag = nextValueBuffer.array();
+                if (Arrays.equals(CHECKSUM_BYTES, tag)) {
+                    this.hasFoundFinalDelimiter = true;
+                }
+
+                // TODO Make lookup table.
+                this.lastDeserializedTag = Integer.parseInt(new String(tag));
+
+                switch (this.parsingState) {
+                case REPEATING_GROUP:
+                    if (Arrays.binarySearch(this.repeatingGroupTags, this.lastDeserializedTag) < 0) {
+                        if (this.hasFoundNumberOfRepeatingGroupsTag()) {
+                            this.handleNewRepeatingGroup();
+                        } else {
+                            this.parsingState = ParsingState.NON_REPEATING_GROUP;
+                        }
+                    }
+
+                    break;
+                case NON_REPEATING_GROUP:
+                    if (this.hasFoundNumberOfRepeatingGroupsTag()) {
+                        this.handleNewRepeatingGroup();
+                    }
+
+                    break;
+                }
+            } else if (Fields.DELIMITER == this.nextFlagByte) {
+                this.nextFlagByte = Fields.EQUAL_SIGN;
+
+                final byte[] value = nextValueBuffer.array();
+
+                switch (this.parsingState) {
+                case REPEATING_GROUP:
+                    if (this.hasFoundNumberOfRepeatingGroupsTag) {
+                        this.hasFoundNumberOfRepeatingGroupsTag = false;
+
+                        this.repeatingGroupBuilder.setNumberOfRepeatingGroupsTag(this.lastDeserializedTag);
+                    } else {
+                        this.repeatingGroupBuilder.addField(this.lastDeserializedTag, value);
+
+                        // TODO Hook in repeating groups into inbound FIX message builder.
+                    }
+
+                    break;
+                case NON_REPEATING_GROUP:
+                    this.inboundFixMessageBuilder.addField(this.lastDeserializedTag, value);
+                    if (this.hasFoundFinalDelimiter) {
+                        this.hasFoundFinalDelimiter = false;
+
+                        final InboundFixMessage inboundFixMessage =
+                                this.inboundFixMessageBuilder.build(this.repeatingGroupBuilder.build(),
+                                        this.repeatingGroupBuilder.getNumberOfRepeatingGroupTags());
+                        this.reset();
+
+                        return inboundFixMessage;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        // TODO Consider how to better handle this situation.
+        throw new DeserializationException("Unable to deserialize FIX message.");
+    }
+
+    private void handleNewRepeatingGroup() {
+        this.repeatingGroupTags = RepeatingGroups.NUMBER_IN_GROUP_TAGS[this.lastDeserializedTag];
+        this.parsingState = ParsingState.REPEATING_GROUP;
+        this.hasFoundNumberOfRepeatingGroupsTag = true;
+    }
+
+    private boolean hasFoundNumberOfRepeatingGroupsTag() {
+        return null != RepeatingGroups.NUMBER_IN_GROUP_TAGS[this.lastDeserializedTag];
+    }
+
+    private void reset() {
+        this.nextFlagIndex = -1;
+        this.nextFlagByte = Fields.EQUAL_SIGN;
+        this.hasFoundFinalDelimiter = false;
+        this.lastDeserializedTag = -1;
+        this.repeatingGroupTags = null;
+        this.inboundFixMessageBuilder.clear();
+        this.repeatingGroupBuilder.clear();
+    }
+
+    private static enum ParsingState {
+        REPEATING_GROUP, NON_REPEATING_GROUP
+    }
+}
