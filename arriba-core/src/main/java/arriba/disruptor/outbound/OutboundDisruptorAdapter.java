4c7ce04a1f19a221ecf9a23d495d900aa9b1a74c
Michael Diamant <diamant.michael@gmail.com>
2012-01-08 15:39:26 -0500

    Set SessionID in outbound event when processing OutboundFixMessage for proper downstream event processing.

--- arriba-core/src/main/java/arriba/disruptor/outbound/OutboundDisruptorAdapter.java
+++ arriba-core/src/main/java/arriba/disruptor/outbound/OutboundDisruptorAdapter.java
@@ -4,6 +4,7 @@ import arriba.disruptor.MessageToDisruptorAdapter;
 import arriba.disruptor.SessionIdToDisruptorAdapter;
 import arriba.fix.outbound.OutboundFixMessage;
 import arriba.fix.session.SessionId;
+import arriba.fix.session.SessionIds;
 
 public final class OutboundDisruptorAdapter implements
 MessageToDisruptorAdapter<OutboundFixMessage, OutboundEvent>,
@@ -12,7 +13,7 @@ SessionIdToDisruptorAdapter<OutboundEvent> {
     @Override
     public void adapt(final OutboundFixMessage message, final OutboundEvent event) {
         event.setFixMessage(message);
-        event.setSessionId(null);
+        event.setSessionId(SessionIds.newSessionId(message));
         event.setSerializedFixMessage(null);
         event.setResend(false);
     }
