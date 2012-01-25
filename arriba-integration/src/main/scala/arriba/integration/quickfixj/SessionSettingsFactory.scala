0aa8ccb7e0593ebf733e3e4fa4f9ed59902bb0fa
michaeldiamant <michaeldiamant@users.noreply.github.com>
2012-01-24 21:37:05 -0500

    * Refactor existing acceptor into QuickFixClients class. * Develop QuickFixInitiator to simulate market taker.

---
+++ arriba-integration/src/main/scala/arriba/integration/quickfixj/SessionSettingsFactory.scala
@@ -0,0 +1,43 @@
+package arriba.integration.quickfixj
+import quickfix.SessionSettings
+import quickfix.SessionID
+
+object SessionSettingsFactory {
+
+  def newInitiator(beginString: String, senderCompId: String, targetCompId: String, settings: SessionSettings = new SessionSettings) =
+    createSettings(beginString, senderCompId, targetCompId, settings, "initiator")
+
+  def newAcceptor(beginString: String, senderCompId: String, targetCompId: String, settings: SessionSettings = new SessionSettings) =
+    createSettings(beginString, senderCompId, targetCompId, settings, "acceptor")
+
+  private def createSettings(beginString: String, senderCompId: String, targetCompId: String,
+    settings: SessionSettings, connectionType: String): SessionSettings = {
+    val id = new SessionID(beginString, senderCompId, targetCompId)
+
+    settings.setString(id, "BeginString", id.getBeginString)
+    settings.setString(id, "SenderCompID", id.getSenderCompID)
+    settings.setString(id, "TargetCompID", id.getTargetCompID)
+
+    settings.setString(id, "ConnectionType", connectionType)
+    settings.setString(id, "StartTime", "00:00:00")
+    settings.setString(id, "EndTime", "00:00:00")
+    settings.setString(id, "TimeZone", "America/New_York")
+
+    connectionType match {
+      case "initiator" => {
+        settings.setLong(id, "SocketConnectPort", 8080)
+        settings.setString(id, "SocketConnectHost", "localhost")
+        settings.setLong(id, "HeartBtInt", 30)
+      }
+      case "acceptor" => {
+        settings.setLong(id, "SocketAcceptPort", 8080)
+        settings.setString(id, "SocketAcceptAddress", "localhost")
+      }
+    }
+
+    settings.setString(id, "FileLogPath", "/tmp")
+    settings.setString(id, "FileStorePath", "/tmp")
+
+    settings
+  }
+}
