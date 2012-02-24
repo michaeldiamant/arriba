f78e369cc218150e4c1ecd1021e6c1347f45923d
michaeldiamant <michaeldiamant@users.noreply.github.com>
2012-02-23 19:31:57 -0500

    Forward connected callback upstream.

--- arriba-core/src/main/java/arriba/transport/netty/NewClientSessionHandler.java
+++ arriba-core/src/main/java/arriba/transport/netty/NewClientSessionHandler.java
@@ -6,17 +6,23 @@ import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.group.ChannelGroup;
 
 public class NewClientSessionHandler extends SimpleChannelHandler {
 
     private final List<Channel> channels;
-
-    public NewClientSessionHandler(final List<Channel> channels) {
+    private final ChannelGroup group;
+    
+    public NewClientSessionHandler(final List<Channel> channels, ChannelGroup group) {
         this.channels = channels;
+        this.group = group;
     }
 
     @Override
     public void channelConnected(final ChannelHandlerContext ctx, final ChannelStateEvent e) throws Exception {
+        this.group.add(e.getChannel());
         this.channels.add(e.getChannel());
+
+        super.channelConnected(ctx, e);
     }
 }
