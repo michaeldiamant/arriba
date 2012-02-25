5ad4095e42992db0fc4e047bfe1a482da5063235
michaeldiamant <michaeldiamant@users.noreply.github.com>
2012-02-24 21:21:38 -0500

    Introduce TransportRepository.find() by TransportIdentity.

--- arriba-core/src/main/java/arriba/transport/TransportRepository.java
+++ arriba-core/src/main/java/arriba/transport/TransportRepository.java
@@ -2,9 +2,11 @@ package arriba.transport;
 
 public interface TransportRepository<ID, T> {
 
-    Transport<T> add(ID id, TransportIdentity<T> transport);
+    Transport<T> add(ID id, TransportIdentity<T> identity);
 
-    boolean remove(TransportIdentity<T> transport);
+    boolean remove(TransportIdentity<T> identity);
 
     Transport<T> find(ID id);
+    
+    ID find(TransportIdentity<T> identity);
 }
