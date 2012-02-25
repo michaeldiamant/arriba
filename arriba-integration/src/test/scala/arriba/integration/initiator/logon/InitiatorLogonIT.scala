7c619df0961c2c270b6f612922c7974cc611140f
Michael Diamant <diamant.michael@gmail.com>
2012-02-25 09:38:10 -0500

    Delete sequence number files at the start of each test.

--- arriba-integration/src/test/scala/arriba/integration/initiator/logon/InitiatorLogonIT.scala
+++ arriba-integration/src/test/scala/arriba/integration/initiator/logon/InitiatorLogonIT.scala
@@ -5,9 +5,17 @@ import arriba.integration.runner.ClientWizard
 import arriba.integration.quickfixj._
 import quickfix.Message
 import quickfix.field.{Password, Username, MsgType}
-import arriba.fix.inbound.messages.Logon
+import arriba.fix.inbound.messages.{Logout, Logon}
+import org.apache.commons.io.FileUtils
+import java.io.{File, FileFilter}
+import org.specs2.specification.BeforeExample
 
-class InitiatorLogonIT extends SpecificationWithJUnit {
+class InitiatorLogonIT extends SpecificationWithJUnit with BeforeExample {
+
+  def before() {
+    val deletes = new File("/tmp").listFiles().filter(_.getName.contains(".seqnums"))
+    deletes.foreach(file => FileUtils.forceDelete(file))
+  }
 
   "Arriba initiator sending logon" should {
 
@@ -33,6 +41,49 @@ class InitiatorLogonIT extends SpecificationWithJUnit {
       arribaInitiator handle {
         case message: Logon => // Just ensuring a logon response is received.
       }
+
+      wizard start()
+    }
+
+    "expect disconnect when logging in with sequence number that is too low" >> {
+      implicit val wizard = new ClientWizard
+      val session = FixSession("FIX.4.4", "INITIATOR", "ACCEPTOR", "user", "pw")
+
+      implicit val firstArribaInitiator = new ArribaStub(Initiator)
+      implicit val qFixAcceptor = new QuickFixStub(Acceptor)
+
+      firstArribaInitiator addSession session
+      qFixAcceptor addSession session.copy(senderCompId = session.targetCompId, targetCompId = session.senderCompId)
+
+      qFixAcceptor handle {
+        case message: Message if message.getHeader.getString(MsgType.FIELD).equals(MsgType.LOGON) =>
+      }
+
+      firstArribaInitiator handle {
+        case message: Logon =>
+      }
+
+      val secondArribaInitiator = new ArribaStub(Initiator)
+      wizard += (latch => {
+        () => {
+          firstArribaInitiator stop()
+
+          Thread.sleep(500)
+
+          secondArribaInitiator addSession session
+          secondArribaInitiator start()
+
+          latch.countDown()
+        }
+      })
+
+      wizard += (latch => {
+        () => {
+          Thread.sleep(500) // Waiting for 35=5 from acceptor.  Message will not be forwarded so no verification can occur.
+          secondArribaInitiator stop()
+          latch.countDown()
+        }
+      })
 
       wizard start()
     }
